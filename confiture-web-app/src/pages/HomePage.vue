7d31f6f6d7c539b268cc0b00c2061438556d0e49
Adrien Boutigny <adrien@slash-tmp.dev>
2022-07-18 16:28:02 +0200

    feat(app): add homepage

--- confiture-web-app/src/pages/HomePage.vue
+++ confiture-web-app/src/pages/HomePage.vue
@@ -1,15 +1,38 @@
 <template>
-  <h1>This is the home page</h1>
-  <section class="fr-accordion">
-    <h3 class="fr-accordion__title">
-      <button
-        class="fr-accordion__btn"
-        aria-expanded="false"
-        aria-controls="accordion-106"
-      >
-        Qui est le chien le plus mignon ?
-      </button>
-    </h3>
-    <div id="accordion-106" class="fr-collapse">Odette ❤️</div>
+  <section>
+    <h1>Je démarre un nouvel audit</h1>
+    <div class="fr-card fr-enlarge-link fr-card--horizontal">
+      <div class="fr-card__body">
+        <div class="fr-card__content">
+          <p>
+            Avant de démarrer l’audit vous passerez par 2 étapes afin de saisir
+            :
+          </p>
+          <ul>
+            <li>Les information générales de la démarche à auditer</li>
+            <li>Les informations générales de l’audit lui-même</li>
+          </ul>
+          <p>
+            ⏱️ Ces 2 étapes vous prendront environ 15 min. Vous pourrez ensuite
+            faire votre audit et le vérifier avant de le livrer à
+            l’administration.
+          </p>
+          <RouterLink to="/nouvel-audit" class="fr-btn fr-btn--sm">
+            Je démarre mon audit
+          </RouterLink>
+        </div>
+      </div>
+
+      <!-- TODO: Replace with correct illustration -->
+      <div class="fr-card__header">
+        <div class="fr-card__img">
+          <img
+            class="fr-responsive-img"
+            src="https://picsum.photos/id/123/600/400"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   </section>
 </template>
