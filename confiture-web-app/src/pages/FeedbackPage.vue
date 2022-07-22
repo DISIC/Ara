e2cefe9f4fa211e23a3b3002c7723443d59d8ef5
Quentin Bellanger <hello@quentin-bellanger.com>
2022-07-22 17:19:29 +0200

    design feedback page (#4)

---
+++ confiture-web-app/src/pages/FeedbackPage.vue
@@ -0,0 +1,331 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const availableJobs = [
+  "Designer",
+  "Développeur",
+  "Chef de projet",
+  "Chef de produit",
+  "Décideur",
+  "Référent accessibilité",
+  "Autre",
+];
+
+const easeOfUse = ref("");
+const languageLevel = ref("");
+const generalFeedback = ref("");
+const changes = ref("");
+const contact = ref();
+const name = ref("");
+const email = ref("");
+const jobs = ref([]);
+
+const showSuccess = ref(false);
+
+/**
+ * Submit form and display success notice
+ */
+function submitFeedback() {
+  // TODO: connect feedback to API
+  const data = {
+    easeOfUse: easeOfUse.value,
+    languageLevel: languageLevel.value,
+    generalFeedback: generalFeedback.value,
+    changes: changes.value,
+    contact: contact.value,
+    name: name.value,
+    email: email.value,
+    jobs: jobs.value,
+  };
+  console.log(data);
+
+  showSuccess.value = true;
+}
+</script>
+
+<template>
+  <h1 class="fr-mb-6w">Donner mon avis</h1>
+  <div aria-live="polite" aria-atomic="true" role="alert">
+    <template v-if="showSuccess">
+      <p class="fr-h3">
+        <span class="fr-icon-checkbox-circle-fill fr-fi--lg check-icon" />
+        Votre avis a bien été envoyé
+      </p>
+      <p>
+        Nous vous remercions pour le temps que vous avez pris, tous les avis
+        partagés seront étudiés.
+      </p>
+      <!-- TODO: set page name -->
+      <RouterLink
+        class="fr-link fr-icon-arrow-left-line fr-link--icon-left"
+        :to="{ name: 'help' }"
+      >
+        Retourner à la page [nom de la page]
+      </RouterLink>
+    </template>
+  </div>
+  <form v-if="!showSuccess" class="content" @submit.prevent="submitFeedback">
+    <p class="fr-text--sm mandatory-notice">
+      Sauf mentions contraires, tous les champs sont obligatoires
+    </p>
+
+    <!-- TODO: use custom radio -->
+    <section class="fr-form-group">
+      <fieldset class="fr-fieldset fr-fieldset--inline">
+        <legend class="fr-fieldset__legend fr-text--regular">
+          <h2 class="fr-text--md fr-text--bold fr-mb-1v">
+            Ce site est-il facile à utiliser ?
+          </h2>
+        </legend>
+        <div class="fr-fieldset__content">
+          <div class="fr-radio-group">
+            <input
+              id="ease-of-use-yes"
+              v-model="easeOfUse"
+              type="radio"
+              name="easeOfUse"
+              value="yes"
+              required
+            />
+            <label class="fr-label" for="ease-of-use-yes">Oui</label>
+          </div>
+          <div class="fr-radio-group">
+            <input
+              id="ease-of-use-medium"
+              v-model="easeOfUse"
+              type="radio"
+              name="easeOfUse"
+              value="medium"
+              required
+            />
+            <label class="fr-label" for="ease-of-use-medium">Moyen</label>
+          </div>
+          <div class="fr-radio-group">
+            <input
+              id="ease-of-use-no"
+              v-model="easeOfUse"
+              type="radio"
+              name="easeOfUse"
+              value="no"
+              required
+            />
+            <label class="fr-label" for="ease-of-use-no">Non</label>
+          </div>
+        </div>
+      </fieldset>
+    </section>
+
+    <section class="fr-form-group">
+      <fieldset class="fr-fieldset fr-fieldset--inline">
+        <legend class="fr-fieldset__legend fr-text--regular">
+          <h2 class="fr-text--md fr-text--bold fr-mb-1v">
+            Le langage employé est-il facile à comprendre ?
+          </h2>
+        </legend>
+        <div class="fr-fieldset__content">
+          <div class="fr-radio-group">
+            <input
+              id="language-level-yes"
+              v-model="languageLevel"
+              type="radio"
+              name="languageLevel"
+              value="yes"
+              required
+            />
+            <label class="fr-label" for="language-level-yes">Oui</label>
+          </div>
+          <div class="fr-radio-group">
+            <input
+              id="language-level-medium"
+              v-model="languageLevel"
+              type="radio"
+              name="languageLevel"
+              value="medium"
+              required
+            />
+            <label class="fr-label" for="language-level-medium">Moyen</label>
+          </div>
+          <div class="fr-radio-group">
+            <input
+              id="language-level-no"
+              v-model="languageLevel"
+              type="radio"
+              name="languageLevel"
+              value="no"
+              required
+            />
+            <label class="fr-label" for="language-level-no">Non</label>
+          </div>
+        </div>
+      </fieldset>
+    </section>
+
+    <section class="fr-input-group fr-mb-4w narrow-content">
+      <label class="fr-label" for="general-feedback">
+        <h2 class="fr-text--md fr-text--bold fr-mb-1v">
+          Quelles sont vos remarques générales ?
+        </h2>
+        <span class="fr-hint-text"
+          >Vous pouvez détailler les problèmes que vous avez rencontrés ou
+          simplement nous dire ce qui vous plaît ou vous déplait</span
+        >
+      </label>
+      <textarea
+        id="general-feedback"
+        v-model="generalFeedback"
+        class="fr-input"
+        required
+      />
+    </section>
+
+    <section class="fr-input-group fr-mb-4w narrow-content">
+      <label class="fr-label" for="changes">
+        <h2 class="fr-text--md fr-text--bold fr-mb-1v">
+          Que changeriez-vous ou ajouteriez-vous ?
+        </h2>
+
+        <span class="fr-hint-text">
+          Vous pouvez exprimer vos besoins ou proposer vos idées d’amélioration
+        </span>
+      </label>
+      <textarea id="changes" v-model="changes" class="fr-input" required />
+    </section>
+
+    <section>
+      <div class="fr-form-group fr-mb-4w narrow-content">
+        <fieldset class="fr-fieldset">
+          <legend class="fr-fieldset__legend fr-text--regular">
+            <h2 class="fr-text--md fr-text--bold fr-mb-1v">
+              Si besoin, accepteriez-vous d’être contacté par l’équipe de
+              conception ?
+            </h2>
+            <span class="fr-hint-text"
+              >Nous pourrions vous soliciter pour des tests utilisateur ou pour
+              échanger sur vos besoins</span
+            >
+          </legend>
+          <div class="fr-fieldset__content">
+            <div class="fr-radio-group">
+              <input
+                id="contact-yes"
+                v-model="contact"
+                type="radio"
+                name="contact"
+                value="yes"
+                required
+              />
+              <label class="fr-label" for="contact-yes">Oui</label>
+            </div>
+            <div class="fr-radio-group">
+              <input
+                id="contact-no"
+                v-model="contact"
+                type="radio"
+                name="contact"
+                value="no"
+                required
+              />
+              <label class="fr-label" for="contact-no">Non</label>
+            </div>
+          </div>
+        </fieldset>
+      </div>
+
+      <template v-if="contact === 'yes'">
+        <div class="fr-input-group narrow-content">
+          <label class="fr-label" for="name">
+            Je m’appelle
+
+            <span class="fr-hint-text">Au format : Prénom Nom</span>
+          </label>
+          <input
+            id="name"
+            v-model="name"
+            type="text"
+            class="fr-input"
+            required
+          />
+        </div>
+
+        <div class="fr-input-group narrow-content">
+          <label class="fr-label" for="email">
+            Mon adresse e-mail
+
+            <span class="fr-hint-text">Au format : prenom@domaine.fr</span>
+          </label>
+          <input
+            id="email"
+            v-model="email"
+            type="email"
+            class="fr-input"
+            required
+          />
+        </div>
+
+        <div class="fr-form-group narrow-content">
+          <fieldset class="fr-fieldset">
+            <legend class="fr-fieldset__legend fr-text--regular">
+              Je suis
+            </legend>
+            <div class="fr-fieldset__content">
+              <div
+                v-for="job in availableJobs"
+                :key="job"
+                class="fr-checkbox-group"
+              >
+                <input
+                  :id="`job-${job}`"
+                  v-model="jobs"
+                  type="checkbox"
+                  :name="`job-${job}`"
+                  :value="job"
+                />
+                <label class="fr-label" :for="`job-${job}`">{{ job }}</label>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </template>
+    </section>
+
+    <p class="fr-text--sm fr-mb-5w data-notice">
+      Les données recueillies sur ce formulaire sont traitées par les équipes de
+      la DINUM. Elles nous permettent d’améliorer ce site et de vous recontacter
+      si besoin par e-mail pour approndires vos remarques, ou vous solliciter
+      pour des tests usagers dans le cas où vous auriez accepté d’être
+      recontacté. Conformément à la règlementation, vous disposez d’un droit
+      d’opposition et d’un droit à la limitation du traitement de données vous
+      concernant, ainsi que d’un droit d’accès, de rectification, de portabilité
+      et d’effacement de vos données. Vous pouvez exercer vos droits en nous
+      écrivant à l’adresse électronique suivante :
+      contact@design.numerique.gouv.fr.
+    </p>
+    <button class="fr-btn fr-mb-5w" type="submit">Envoyer mon avis</button>
+  </form>
+
+  <a
+    v-if="!showSuccess"
+    class="fr-link fr-icon-arrow-up-fill fr-link--icon-left"
+    href="#header"
+    >Haut de page</a
+  >
+</template>
+
+<style scoped>
+.content {
+  max-width: 49.5rem;
+}
+
+.narrow-content {
+  max-width: 30rem;
+}
+
+.mandatory-notice,
+.data-notice {
+  color: var(--text-mention-grey);
+}
+
+.check-icon {
+  color: var(--text-default-success);
+}
+</style>
