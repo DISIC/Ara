09d73c247a0094f4b9d3573d2807559c2131b813
Quentin Bellanger <hello@quentin-bellanger.com>
2022-07-22 10:42:36 +0200

    add a11y, legal and personal data pages

---
+++ confiture-web-app/src/pages/PersonalDataPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="fr-my-0 fr-mx-auto content">
+    <h1>Données personnelles</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+      reiciendis adipisci provident voluptate error accusamus, voluptas quia
+      deserunt ad quisquam necessitatibus mollitia voluptates non fugit
+      veritatis? Illum quaerat aperiam modi!
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.content {
+  max-width: 49.5rem;
+}
+</style>
--- confiture-web-app/src/router.ts
+++ confiture-web-app/src/router.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
 import SiteMapPage from "./pages/SiteMapPage.vue";
+import AccessibilityPage from "./pages/AccessibilityPage.vue";
+import LegalPage from "./pages/LegalPage.vue";
+import PersonalDataPage from "./pages/PersonalDataPage.vue";
 import NotFoundPage from "./pages/NotFoundPage.vue";
 import NewAuditPage from "./pages/NewAuditPage.vue";
 import AuditPage from "./pages/AuditPage.vue";
@@ -17,6 +20,21 @@ const router = createRouter({
       path: "/plan-du-site",
       name: "site-map",
       component: SiteMapPage,
+    },
+    {
+      path: "/accessibilite",
+      name: "accessibility",
+      component: AccessibilityPage,
+    },
+    {
+      path: "/mentions-legales",
+      name: "legal",
+      component: LegalPage,
+    },
+    {
+      path: "/donnees-personnelles",
+      name: "personal-data",
+      component: PersonalDataPage,
     },
     {
       path: "/audits/nouveau",
