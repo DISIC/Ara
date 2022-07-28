12861061477872832e61ce34661dc704d066f50b
Adrien Boutigny <adrien@slash-tmp.dev>
2022-07-28 11:10:06 +0200

    move audit form to component

--- confiture-web-app/src/api/createAudit.ts
+++ confiture-web-app/src/api/createAudit.ts
@@ -1,50 +1,13 @@
 import ky from "ky";
-
-export interface CreateAuditData {
-  procedure: string;
-  procedureUrl: string;
-
-  initiator: string;
-
-  contactName?: string;
-  contactEmail: string;
-  contactFormUrl: string;
-
-  recipients: Array<{
-    name: string;
-    email: string;
-  }>;
-
-  auditorName: string;
-  auditorEmail: string;
-}
-
-interface CreateAuditResponse {
-  id: number;
-  editUniqueId: string;
-  consultUniqueId: string;
-  initiator: string;
-  procedure: string;
-  url: string;
-  contactName: string | null;
-  contactEmail: string;
-  contactFormUrl: string;
-  auditorName: string;
-  auditorEmail: string;
-  recipients: Array<{
-    id: number;
-    name: string;
-    email: string;
-  }>;
-}
+import { Audit, CreateAuditRequestData } from "../types";
 
 export async function createAudit(
-  data: CreateAuditData
-): Promise<CreateAuditResponse> {
+  data: CreateAuditRequestData
+): Promise<Audit> {
   const response = (await ky
     .post("/api/audits", {
       json: data,
     })
-    .json()) as CreateAuditResponse;
+    .json()) as Audit;
   return response;
 }
--- confiture-web-app/src/types.ts
+++ confiture-web-app/src/types.ts
@@ -1,5 +1,31 @@
+export interface AuditRecipent {
+  id: number;
+  name: string;
+  email: string;
+}
+
 export interface Audit {
   id: number;
-  uniqueId: string;
-  procedure: string;
+  editUniqueId: string;
+  consultUniqueId: string;
+
+  initiator: string;
+
+  procedureName: string;
+  procedureUrl: string;
+
+  contactName: string | null;
+  contactEmail: string;
+  contactFormUrl: string;
+
+  auditorName: string;
+  auditorEmail: string;
+
+  recipients: AuditRecipent[];
 }
+
+/** Audit type but without the generated IDs */
+export type CreateAuditRequestData = Omit<
+  Audit,
+  "id" | "editUniqueId" | "consultUniqueId" | "recipients"
+> & { recipients: Omit<AuditRecipent, "id">[] };
