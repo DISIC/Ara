d9180e65f62bceac2b8b0a366ac8edc69ca1246d
Quentin Bellanger <hello@quentin-bellanger.com>
2022-07-28 11:08:12 +0200

    correct home page structure

--- confiture-web-app/src/pages/HomePage.vue
+++ confiture-web-app/src/pages/HomePage.vue
@@ -1,36 +1,41 @@
 <template>
   <section>
-    <h1>Je démarre un nouvel audit</h1>
-    <div class="fr-card fr-enlarge-link fr-card--horizontal">
-      <div class="fr-card__body">
-        <div class="fr-card__content">
-          <p>
-            Avant de démarrer l’audit vous passerez par 2 étapes afin de saisir
-            :
-          </p>
-          <ul>
-            <li>Les information générales de la démarche à auditer</li>
-            <li>Les informations générales de l’audit lui-même</li>
-          </ul>
-          <p>
-            ⏱️ Ces 2 étapes vous prendront environ 15 min. Vous pourrez ensuite
-            faire votre audit et le vérifier avant de le livrer à
-            l’administration.
-          </p>
-          <RouterLink to="/audits/nouveau" class="fr-btn fr-btn--sm">
-            Je démarre mon audit
-          </RouterLink>
-        </div>
-      </div>
+    <h1>Audit d’accessibilité numérique</h1>
+    <p>
+      Avant de démarrer un nouvel audit vous passerez par 2 étapes afin de
+      saisir :
+    </p>
+    <ul>
+      <li>Les information générales du site à auditer</li>
+      <li>Les informations générales de l’audit lui-même</li>
+    </ul>
+    <p class="fr-mb-4w">
+      ⏱️ Ces 2 étapes vous prendront environ 15 min. Vous pourrez ensuite faire
+      votre audit.
+    </p>
 
-      <!-- TODO: Replace with correct illustration -->
-      <div class="fr-card__header">
-        <div class="fr-card__img">
-          <img
-            class="fr-responsive-img"
-            src="https://picsum.photos/id/123/600/400"
-            alt=""
-          />
+    <div class="fr-grid-row">
+      <div class="fr-col-12 fr-col-md-6">
+        <div class="fr-card fr-card--horizontal fr-card--horizontal-tier">
+          <div class="fr-card__body">
+            <div class="fr-card__content">
+              <p class="fr-h4">Je démarre un nouvel audit</p>
+              <RouterLink to="/audits/nouveau" class="fr-btn fr-btn--sm">
+                C’est parti !
+              </RouterLink>
+            </div>
+          </div>
+
+          <!-- TODO: Replace with correct illustration -->
+          <div class="fr-card__header">
+            <div class="fr-card__img">
+              <img
+                class="fr-responsive-img"
+                src="https://picsum.photos/id/123/600/400"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
