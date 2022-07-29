4640b9d6d0597e91ae8b022a9e3e45b354aa4083
Quentin Bellanger <hello@quentin-bellanger.com>
2022-07-29 14:41:50 +0200

    update home page content

--- confiture-web-app/src/pages/HomePage.vue
+++ confiture-web-app/src/pages/HomePage.vue
@@ -14,30 +14,8 @@
       votre audit.
     </p>
 
-    <div class="fr-grid-row">
-      <div class="fr-col-12 fr-col-md-6">
-        <div class="fr-card fr-card--horizontal fr-card--horizontal-tier">
-          <div class="fr-card__body">
-            <div class="fr-card__content">
-              <p class="fr-h4">Je démarre un nouvel audit</p>
-              <RouterLink to="/audits/nouveau" class="fr-btn fr-btn--sm">
-                C’est parti !
-              </RouterLink>
-            </div>
-          </div>
-
-          <!-- TODO: Replace with correct illustration -->
-          <div class="fr-card__header">
-            <div class="fr-card__img">
-              <img
-                class="fr-responsive-img"
-                src="https://picsum.photos/id/123/600/400"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <RouterLink :to="{ name: 'new-audit-step-one' }" class="fr-btn">
+      Je démarre un audit
+    </RouterLink>
   </section>
 </template>
