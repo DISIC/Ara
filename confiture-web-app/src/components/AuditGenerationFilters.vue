4edf8ec5a382c7e15feb8138fe87dd28178ce99d
Quentin Bellanger <hello@quentin-bellanger.com>
2022-08-04 16:02:36 +0200

    design topic filters

--- confiture-web-app/src/components/AuditGenerationFilters.vue
+++ confiture-web-app/src/components/AuditGenerationFilters.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 
 defineProps<{
   resultsCount: number;
+  topics: { title: string; value: number }[];
 }>();
 const emit = defineEmits(["search"]);
 
@@ -69,7 +70,71 @@ async function onSearch() {
     </button>
   </template>
 
-  <p class="fr-label fr-text--bold fr-mt-4w fr-mb-2w" for="">
-    Thématiques de critères
-  </p>
+  <div class="fr-form-group fr-mt-4w">
+    <fieldset class="fr-fieldset">
+      <legend class="fr-fieldset__legend fr-text--regular fr-text--bold">
+        Thématiques de critères
+      </legend>
+      <ol class="fr-fieldset__content fr-pl-0">
+        <li
+          v-for="(topic, i) in topics"
+          :key="i"
+          class="fr-mb-3w topic-filter"
+          :style="{ '--topic-filter-value': topic.value + '%' }"
+        >
+          <div class="fr-checkbox-group topic-filter-checkbox">
+            <input :id="`topic-filter-${i}`" type="checkbox" />
+            <label class="fr-label fr-pb-0" :for="`topic-filter-${i}`">
+              {{ i + 1 }}. {{ topic.title }}
+            </label>
+          </div>
+          <span class="fr-text--sm fr-m-0 fr-ml-1w topic-filter-value"
+            >{{ topic.value }}%</span
+          >
+          <div class="topic-filter-progress" />
+        </li>
+      </ol>
+    </fieldset>
+  </div>
 </template>
+
+<style scoped>
+.topic-filter {
+  --topic-filter-offset: 2rem;
+
+  display: grid;
+  gap: 0.5rem 0;
+  grid-template-columns: var(--topic-filter-offset) 1fr auto;
+  grid-template-rows: 1fr 0.25rem;
+  grid-template-areas:
+    "checkbox checkbox value"
+    "... progress progress";
+}
+
+.topic-filter-checkbox {
+  grid-area: checkbox;
+}
+
+.topic-filter-value {
+  grid-area: value;
+  /* TODO: only FF supports this but degrades gracefully, is ok? */
+  align-self: last baseline;
+}
+
+.topic-filter-progress {
+  grid-area: progress;
+  background-color: var(--background-contrast-grey);
+  position: relative;
+}
+
+.topic-filter-progress::after {
+  background-color: var(--background-flat-success);
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  transition: width 1s ease;
+  width: var(--topic-filter-value);
+}
+</style>
--- confiture-web-app/src/pages/edit/EditAuditStepThreePage.vue
+++ confiture-web-app/src/pages/edit/EditAuditStepThreePage.vue
@@ -16,6 +16,22 @@ function validateAudit() {
 function search(value: string) {
   console.log("search: " + value);
 }
+
+const topics = ref([
+  { title: "Images", value: 25 },
+  { title: "Cadres", value: 12 },
+  { title: "Couleurs", value: 3 },
+  { title: "Multimédia", value: 0 },
+  { title: "Tableaux", value: 12 },
+  { title: "Liens", value: 0 },
+  { title: "Scripts", value: 88 },
+  { title: "Éléments obligatoires", value: 100 },
+  { title: "Structuration de l'information", value: 67 },
+  { title: "Présentation de l'information", value: 0 },
+  { title: "Formulaires", value: 23 },
+  { title: "Navigation", value: 4 },
+  { title: "Consultation", value: 56 },
+]);
 </script>
 
 <template>
@@ -29,7 +45,11 @@ function search(value: string) {
 
   <div class="fr-grid-row fr-grid-row--gutters">
     <div class="fr-col-12 fr-col-md-3">
-      <AuditGenerationFilters :results-count="21" @search="search" />
+      <AuditGenerationFilters
+        :results-count="21"
+        :topics="topics"
+        @search="search"
+      />
     </div>
     <div class="fr-col-12 fr-col-md-9">
       <AuditGenerationCriteria />
