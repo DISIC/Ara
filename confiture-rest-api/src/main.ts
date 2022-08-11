8f3a0ee45ef05ca70aef3e731851f97ddb9f8878
Adrien Boutigny <adrien@slash-tmp.dev>
2022-08-11 02:24:30 +0200

    feat(back): add logger in dev mode

--- confiture-rest-api/src/main.ts
+++ confiture-rest-api/src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+  }
+
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT || 4000);
