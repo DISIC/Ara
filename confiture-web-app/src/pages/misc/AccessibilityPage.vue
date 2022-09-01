fd6209f17f0bf4dee739e054f8cedebb76ab7e2f
Quentin Bellanger <hello@quentin-bellanger.com>
2022-09-01 15:50:44 +0200

    write accessibility page content

--- confiture-web-app/src/pages/misc/AccessibilityPage.vue
+++ confiture-web-app/src/pages/misc/AccessibilityPage.vue
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
 <script setup lang="ts">
 import PageMeta from "../../components/PageMeta";
 </script>
 
+=======
+<!--
+  TODO: complete all content once the audit will be done.
+ -->
+>>>>>>> 6cb3e29 (write accessibility page content)
 <template>
   <PageMeta
     title="Accessibilité"
@@ -10,10 +16,199 @@ import PageMeta from "../../components/PageMeta";
   <section class="fr-my-0 fr-mx-auto content">
     <h1>Déclaration d’accessibilité</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
-      reiciendis adipisci provident voluptate error accusamus, voluptas quia
-      deserunt ad quisquam necessitatibus mollitia voluptates non fugit
-      veritatis? Illum quaerat aperiam modi!
+      <strong>[Nom de l’entité]</strong> s’engage à rendre ses sites internet,
+      intranet, extranet et ses progiciels accessibles (et ses applications
+      mobiles et mobilier urbain numérique) conformément à l’article 47 de la
+      loi n°2005-102 du 11 février 2005.
+    </p>
+
+    <p>
+      À cette fin, <strong>[Nom de l’entité]</strong> met en œuvre la stratégie
+      et les actions suivantes :
+    </p>
+
+    <ul>
+      <li>
+        Schéma pluriannuel de mise en accessibilité 2022-2024
+        <strong>[url]</strong> ;
+      </li>
+      <li>Actions réalisées en 2020-2021 <strong>[url]</strong> ;</li>
+      <li>Plan d’actions 2022-2024 <strong>[url]</strong>.</li>
+    </ul>
+
+    <p>
+      Cette déclaration d’accessibilité s’applique à
+      <strong>[www.nomdusite.gouv.fr]</strong>.
+    </p>
+
+    <nav
+      class="fr-summary fr-mb-6w"
+      role="navigation"
+      aria-labelledby="fr-summary-title"
+    >
+      <p id="fr-summary-title" class="fr-summary__title">Sommaire</p>
+      <ol class="fr-summary__list">
+        <li>
+          <a class="fr-summary__link" href="#etat-conformite"
+            >État de conformité</a
+          >
+        </li>
+        <li>
+          <a class="fr-summary__link" href="#resultats-tests"
+            >Résultats des tests</a
+          >
+        </li>
+        <li>
+          <a class="fr-summary__link" href="#contenus-non-accessibles"
+            >Contenus non accessibles</a
+          >
+        </li>
+        <li>
+          <a class="fr-summary__link" href="#etablissement-declaration"
+            >Établissement de cette déclaration d'accessibilité</a
+          >
+        </li>
+        <li>
+          <a class="fr-summary__link" href="#contact"
+            >Retour d'information et contact</a
+          >
+        </li>
+        <li>
+          <a class="fr-summary__link" href="#recours">Voies de recours</a>
+        </li>
+      </ol>
+    </nav>
+
+    <h2 id="etat-conformite">État de conformité</h2>
+
+    <p>
+      <strong>[Nom du site (url du site)]</strong> est
+      <strong>non/partiellement/totalement</strong> conforme avec le référentiel
+      général d’amélioration de l’accessibilité (RGAA), version 4 en raison des
+      non-conformités et des dérogations énumérées ci-dessous.
+    </p>
+
+    <h2 id="resultats-tests">Résultats des tests</h2>
+    <p>
+      L’audit de conformité réalisé par [nom de l’entité qui a réalisé l’audit]
+      révèle que :
+    </p>
+
+    <ul class="fr-mb-6w">
+      <li>
+        <strong>[nn]</strong>% des critères du RGAA version 4 sont respectés ;
+      </li>
+      <li>
+        (Facultatif) Le taux moyen de conformité du site s’élève à
+        <strong>[nn]</strong>% ;
+      </li>
+      <li>
+        (Facultatif) Accès à la grille d’audit RGAA <strong>[url]</strong> pour
+        télécharger la grille d’audit.
+      </li>
+    </ul>
+
+    <h2 id="contenus-non-accessibles">Contenus non accessibles</h2>
+    <h3>Non-conformités</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius vero iusto
+      aliquid amet placeat. Earum deserunt voluptatem sapiente est sunt aperiam
+      magni adipisci repudiandae, rem repellat eos illum quos officia.
+    </p>
+
+    <h3>Dérogations pour charge disproportionnée</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat minima
+      et impedit, perferendis fugiat suscipit tempore vero molestias deserunt
+      facere magnam nulla veritatis autem ratione consequuntur pariatur, sit
+      soluta cum.
+    </p>
+
+    <h3>Contenus non soumis à l’obligation d’accessibilité</h3>
+    <p class="fr-mb-6w">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus beatae iure
+      quibusdam modi hic commodi odio. Porro magni nisi architecto at, ipsa
+      maiores debitis labore est voluptatem tempora atque! Incidunt.
+    </p>
+
+    <h2 id="etablissement-declaration">
+      Établissement de cette déclaration d'accessibilité
+    </h2>
+    <p>
+      Cette déclaration a été établie le <strong>[JJ/MM/AAAA]</strong>. Elle a
+      été mise à jour le <strong>[JJ/MM/AAAA]</strong>.
+    </p>
+
+    <h3>Technologies utilisées pour la réalisation l’audit</h3>
+    <ul class="fr-mb-3w">
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+
+    <h3>Environnement de test</h3>
+    <p>
+      Les vérifications de restitution de contenus ont été réalisées sur la base
+      de la combinaison fournie par la base de référence du RGAA, avec les
+      versions suivantes :
+    </p>
+    <ul class="fr-mb-3w">
+      <li>Firefox et NVDA</li>
+      <li>Safari et VoiceOver</li>
+    </ul>
+
+    <h3>Outils pour évaluer l’accessibilité</h3>
+    <ul class="fr-mb-3w">
+      <li>Bla bla</li>
+      <li>Bla blla</li>
+      <li>Bla bla bla</li>
+    </ul>
+
+    <h3>Pages du site ayant fait l’objet de la vérification de conformité</h3>
+    <ul class="fr-mb-6w">
+      <li>Bla bla</li>
+      <li>Bla blla</li>
+      <li>Bla bla bla</li>
+    </ul>
+
+    <h2 id="contact">Retour d'information et contact</h2>
+    <p>
+      Si vous n’arrivez pas à accéder à un contenu ou à un service, vous pouvez
+      contacter le responsable de [nom du site] pour être orienté vers une
+      alternative accessible ou obtenir le contenu sous une autre forme.
+    </p>
+
+    <ul class="fr-mb-6w">
+      <li>
+        Envoyer un message <strong>[url du formulaire en ligne]</strong> ;
+      </li>
+      <li>
+        Contacter
+        <strong
+          >[Nom de l’entité responsable du service en ligne et
+          coordonnées]</strong
+        >.
+      </li>
+    </ul>
+
+    <h2 id="recours">Voies de recours</h2>
+    <p>
+      Si vous constatez un défaut d’accessibilité vous empêchant d’accéder à un
+      contenu ou une fonctionnalité du site, que vous nous le signalez et que
+      vous ne parvenez pas à obtenir une réponse de notre part, vous êtes en
+      droit de faire parvenir vos doléances ou une demande de saisine au
+      Défenseur des droits.
+    </p>
+    <p>Plusieurs moyens sont à votre disposition :</p>
+
+    <ul>
+      <li>Écrire un message au Défenseur des droits</li>
+      <li>Contacter le délégué du Défenseur des droits dans votre région</li>
+    </ul>
+
+    <p>
+      Envoyer un courrier par la poste (gratuit, ne pas mettre de timbre)
+      Défenseur des droits Libre réponse 71120 75342 Paris CEDEX 07
     </p>
   </section>
 </template>
