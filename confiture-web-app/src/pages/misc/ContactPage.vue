ec27850075eec79d5dd1ae0ab693ae853a088e75
Quentin Bellanger <hello@quentin-bellanger.com>
2022-10-20 10:33:00 +0200

    Page "Contact et contributions" (#154)
    * create contact & contributions page
    
    * hide callout if no report exists
    
    * update condition to show auditor email callout
    
    * create contribution guidelines
    
    * link contributing file
    
    * Fix wording
    
    * Update ContactPage.vue
    
    Co-authored-by: Benoît Dequick <33344370+benoitdequick@users.noreply.github.com>

---
+++ confiture-web-app/src/pages/misc/ContactPage.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import PageMeta from "../../components/PageMeta";
+import { useReportStore } from "../../store";
+
+const reportStore = useReportStore();
+</script>
+
+<template>
+  <PageMeta
+    title="Contactez-nous ou contribuez !"
+    description="Contactez-nous ou contribuez à l’amélioration de Confiture"
+  />
+
+  <section class="fr-mx-auto content">
+    <h1>Contact</h1>
+    <p>
+      Confiture est un outil propulsé par
+      <a
+        class="fr-link"
+        target="_blank"
+        href="https://design.numerique.gouv.fr/"
+        >DesignGouv</a
+      >, le pôle design des services numériques de la
+      <a
+        class="fr-link"
+        target="_blank"
+        href="https://www.numerique.gouv.fr/dinum/"
+        >direction interministérielle du numérique</a
+      >.
+    </p>
+    <p>
+      Vous pouvez nous contacter par e-mail à l’adresse :
+      <strong>rgaa@design.numérique.gouv.fr</strong>.
+    </p>
+
+    <div v-if="reportStore.data" class="fr-callout">
+      <p class="fr-callout__text">
+        Pour toute question relative au rapport d’audit, veuillez contacter
+        l’auditeur ou l’auditrice qui a réalisé cet audit<template
+          v-if="reportStore.data.context.auditorEmail"
+        >
+          à cette adresse électronique
+          <strong>{{ reportStore.data.context.auditorEmail }}</strong></template
+        >.
+      </p>
+    </div>
+
+    <h2>Vos contributions sont les bienvenues !</h2>
+
+    <p>Vous pouvez contribuer à l’amélioration de ce site :</p>
+    <ul>
+      <li>
+        En nous écrivant par e-mail à :
+        <strong>rgaa@design.numerique.gouv.fr</strong>.
+      </li>
+      <li>
+        En créant une issue sur le
+        <a
+          class="fr-link"
+          target="_blank"
+          href="https://github.com/DISIC/confiture"
+          >dépôt</a
+        >. Le guide de contribution est disponible à la racine du dépôt :
+        <a
+          class="fr-link"
+          target="_blank"
+          href="https://github.com/DISIC/confiture/blob/main/CONTRIBUTING.md"
+          >https://github.com/DISIC/confiture/blob/main/CONTRIBUTING.md</a
+        >
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+.content {
+  max-width: 49.5rem;
+}
+</style>
