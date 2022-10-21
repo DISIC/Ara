0bb6ff1eb8145b3ac8c64ff60f4db4ecf9cf2f17
Quentin Bellanger <hello@quentin-bellanger.com>
2022-10-21 11:18:50 +0200

    extract site footer as a sole component

--- confiture-web-app/src/App.vue
+++ confiture-web-app/src/App.vue
@@ -6,6 +6,7 @@ import { useHead } from "@vueuse/head";
 import Breadcrumb, { BreadcrumbLink } from "./components/Breadcrumb.vue";
 import ToastNotification from "./components/ToastNotification.vue";
 import SiteHeader from "./components/SiteHeader.vue";
+import SiteFooter from "./components/SiteFooter.vue";
 
 const route = useRoute();
 
@@ -86,134 +87,7 @@ useHead({
     <RouterView />
   </main>
 
-  <footer id="footer" class="fr-footer" role="contentinfo">
-    <div class="fr-container">
-      <div class="fr-footer__body">
-        <div class="fr-footer__brand fr-enlarge-link">
-          <RouterLink :to="{ name: 'home' }" title="Retour à l’accueil">
-            <p class="fr-logo" title="république française">
-              République<br />
-              Française
-            </p>
-          </RouterLink>
-        </div>
-        <div class="fr-footer__content">
-          <p class="fr-footer__content-desc">
-            Ce site est réalisé par
-            <a
-              href="https://design.numerique.gouv.fr/"
-              title="DesignGouv - nouvelle fenêtre"
-              target="_blank"
-              rel="noreferrer noopener"
-              >DesignGouv</a
-            >, le pôle Design des services numériques de la
-            <a
-              href="https://www.numerique.gouv.fr/dinum/"
-              title="direction interministérielle du numérique - nouvelle fenêtre"
-              rel="noreferrer noopener"
-              target="_blank"
-              >direction interministérielle du numérique</a
-            >.
-          </p>
-          <ul class="fr-footer__content-list">
-            <li class="fr-footer__content-item">
-              <a
-                class="fr-footer__content-link"
-                href="https://legifrance.gouv.fr"
-                target="_blank"
-                rel="noreferrer noopener"
-                title="legifrance.gouv.fr - nouvelle fenêtre"
-                >legifrance.gouv.fr</a
-              >
-            </li>
-            <li class="fr-footer__content-item">
-              <a
-                class="fr-footer__content-link"
-                href="https://gouvernement.fr"
-                target="_blank"
-                rel="noreferrer noopener"
-                title="gouvernement.fr - nouvelle fenêtre"
-                >gouvernement.fr</a
-              >
-            </li>
-            <li class="fr-footer__content-item">
-              <a
-                class="fr-footer__content-link"
-                href="https://service-public.fr"
-                target="_blank"
-                rel="noreferrer noopener"
-                title="service-public.fr - nouvelle fenêtre"
-                >service-public.fr</a
-              >
-            </li>
-            <li class="fr-footer__content-item">
-              <a
-                class="fr-footer__content-link"
-                href="https://data.gouv.fr"
-                target="_blank"
-                rel="noreferrer noopener"
-                title="data.gouv.fr - nouvelle fenêtre"
-                >data.gouv.fr</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="fr-footer__bottom">
-        <ul class="fr-footer__bottom-list">
-          <li class="fr-footer__bottom-item">
-            <RouterLink
-              class="fr-footer__bottom-link"
-              :to="{ name: 'site-map' }"
-            >
-              Plan du site
-            </RouterLink>
-          </li>
-          <li class="fr-footer__bottom-item">
-            <RouterLink
-              class="fr-footer__bottom-link"
-              :to="{ name: 'accessibility' }"
-            >
-              Accessibilité : non conforme
-            </RouterLink>
-          </li>
-          <li class="fr-footer__bottom-item">
-            <RouterLink class="fr-footer__bottom-link" :to="{ name: 'legal' }">
-              Mentions légales
-            </RouterLink>
-          </li>
-          <!-- <li class="fr-footer__bottom-item">
-            <RouterLink
-              class="fr-footer__bottom-link"
-              :to="{ name: 'personal-data' }"
-            >
-              Données personnelles
-            </RouterLink>
-          </li> -->
-          <li class="fr-footer__bottom-item">
-            <RouterLink
-              class="fr-footer__bottom-link"
-              :to="{ name: 'contact' }"
-            >
-              Contact et contributions
-            </RouterLink>
-          </li>
-        </ul>
-        <div class="fr-footer__bottom-copy">
-          <p>
-            Sauf mention contraire, tous les contenus de ce site sont sous
-            <a
-              href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
-              target="_blank"
-              rel="noreferrer noopener"
-              title="licence etalab-2.0 - nouvelle fenêtre"
-              >licence etalab-2.0</a
-            >
-          </p>
-        </div>
-      </div>
-    </div>
-  </footer>
+  <SiteFooter />
 
   <ToastNotification />
 </template>
