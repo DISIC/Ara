2809f623c6bd7c1d10a39bba98a36ae47664c350
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2022-11-10 14:14:00 +0100

    Fix Pages

--- confiture-web-app/src/pages/misc/AccessibilityPage.vue
+++ confiture-web-app/src/pages/misc/AccessibilityPage.vue
@@ -19,157 +19,19 @@ import PageMeta from "../../components/PageMeta";
       accessibles (et ses applications mobiles et mobilier urbain numérique)
       conformément à l’article 47 de la loi n°2005-102 du 11 février 2005.
     </p>
-
-    <!-- <p>
-      À cette fin, la
-      <strong>direction interministérielle du numérique (DINUM)</strong> met en
-      œuvre la stratégie et les actions suivantes :
-    </p> -->
-
-    <!-- <ul>
-      <li>
-        Schéma pluriannuel de mise en accessibilité 2022-2024
-        <strong>[url]</strong> ;
-      </li>
-      <li>Actions réalisées en 2020-2021 <strong>[url]</strong> ;</li>
-      <li>Plan d’actions 2022-2024 <strong>[url]</strong>.</li>
-    </ul> -->
-
     <p>
       Cette déclaration d’accessibilité s’applique à
-      <strong>confiture.netlify.app</strong>.
+      <strong>ara.numerique.gouv.fr</strong>.
     </p>
-
-    <nav
-      class="fr-summary fr-mb-6w"
-      role="navigation"
-      aria-labelledby="fr-summary-title"
-    >
-      <p id="fr-summary-title" class="fr-summary__title">Sommaire</p>
-      <ol class="fr-summary__list">
-        <li>
-          <a class="fr-summary__link" href="#etat-conformite"
-            >État de conformité</a
-          >
-        </li>
-        <!-- <li>
-          <a class="fr-summary__link" href="#resultats-tests"
-            >Résultats des tests</a
-          >
-        </li>
-        <li>
-          <a class="fr-summary__link" href="#contenus-non-accessibles"
-            >Contenus non accessibles</a
-          >
-        </li>
-        <li>
-          <a class="fr-summary__link" href="#etablissement-declaration"
-            >Établissement de cette déclaration d'accessibilité</a
-          >
-        </li> -->
-        <li>
-          <a class="fr-summary__link" href="#contact"
-            >Retour d'information et contact</a
-          >
-        </li>
-        <li>
-          <a class="fr-summary__link" href="#recours">Voies de recours</a>
-        </li>
-      </ol>
-    </nav>
 
     <h2 id="etat-conformite">État de conformité</h2>
 
-    <p>
-      <strong>confiture.netlify.app</strong> est
-      <strong>non conforme</strong> est avec le référentiel général
-      d'amélioration de l'accessibilité (RGAA). Aucun audit n’a été effectué.
-    </p>
+    <p>Aucun audit n’a été encore effectué.<br/>
+    Ara.numerique.gouv.fr est non conforme avec le référentiel général
+      d’amélioration de l’accessibilité (RGAA).</p>
+    <p>Un audit de conformité est bien planifié et le résultat sera publié avant la fin de la version bêta</p>
 
-    <!-- <h2 id="resultats-tests">Résultats des tests</h2>
-    <p>
-      L’audit de conformité réalisé par [nom de l’entité qui a réalisé l’audit]
-      révèle que :
-    </p>
-
-    <ul class="fr-mb-6w">
-      <li>
-        <strong>[nn]</strong>% des critères du RGAA version 4 sont respectés ;
-      </li>
-      <li>
-        (Facultatif) Le taux moyen de conformité du site s’élève à
-        <strong>[nn]</strong>% ;
-      </li>
-      <li>
-        (Facultatif) Accès à la grille d’audit RGAA <strong>[url]</strong> pour
-        télécharger la grille d’audit.
-      </li>
-    </ul>
-
-    <h2 id="contenus-non-accessibles">Contenus non accessibles</h2>
-    <h3>Non-conformités</h3>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius vero iusto
-      aliquid amet placeat. Earum deserunt voluptatem sapiente est sunt aperiam
-      magni adipisci repudiandae, rem repellat eos illum quos officia.
-    </p>
-
-    <h3>Dérogations pour charge disproportionnée</h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat minima
-      et impedit, perferendis fugiat suscipit tempore vero molestias deserunt
-      facere magnam nulla veritatis autem ratione consequuntur pariatur, sit
-      soluta cum.
-    </p>
-
-    <h3>Contenus non soumis à l’obligation d’accessibilité</h3>
-    <p class="fr-mb-6w">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus beatae iure
-      quibusdam modi hic commodi odio. Porro magni nisi architecto at, ipsa
-      maiores debitis labore est voluptatem tempora atque! Incidunt.
-    </p>
-
-    <h2 id="etablissement-declaration">
-      Établissement de cette déclaration d'accessibilité
-    </h2>
-    <p>
-      Cette déclaration a été établie le <strong>[JJ/MM/AAAA]</strong>. Elle a
-      été mise à jour le <strong>[JJ/MM/AAAA]</strong>.
-    </p>
-
-    <h3>Technologies utilisées pour la réalisation l’audit</h3>
-    <ul class="fr-mb-3w">
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-    </ul>
-
-    <h3>Environnement de test</h3>
-    <p>
-      Les vérifications de restitution de contenus ont été réalisées sur la base
-      de la combinaison fournie par la base de référence du RGAA, avec les
-      versions suivantes :
-    </p>
-    <ul class="fr-mb-3w">
-      <li>Firefox et NVDA</li>
-      <li>Safari et VoiceOver</li>
-    </ul>
-
-    <h3>Outils pour évaluer l’accessibilité</h3>
-    <ul class="fr-mb-3w">
-      <li>Bla bla</li>
-      <li>Bla blla</li>
-      <li>Bla bla bla</li>
-    </ul>
-
-    <h3>Pages du site ayant fait l’objet de la vérification de conformité</h3>
-    <ul class="fr-mb-6w">
-      <li>Bla bla</li>
-      <li>Bla blla</li>
-      <li>Bla bla bla</li>
-    </ul> -->
-
-    <h2 id="contact">Retour d'information et contact</h2>
+    <h2 id="contact">Retour d’information et contact</h2>
     <p>
       La direction interministérielle du numérique (DINUM) s’engage à prendre
       les moyens nécessaires afin de donner accès, dans un délai raisonnable,
@@ -183,18 +45,6 @@ import PageMeta from "../../components/PageMeta";
       information et contenu donnés sous une autre forme).
     </p>
 
-    <!-- <ul class="fr-mb-6w">
-      <li>
-        Envoyer un message <strong>[url du formulaire en ligne]</strong> ;
-      </li>
-      <li>
-        Contacter
-        <strong
-          >[Nom de l’entité responsable du service en ligne et
-          coordonnées]</strong
-        >.
-      </li>
-    </ul> -->
 
     <h2 id="recours">Voies de recours</h2>
     <p>
@@ -211,7 +61,7 @@ import PageMeta from "../../components/PageMeta";
         un
         <a
           href="https://www.defenseurdesdroits.fr/nous-contacter"
-          target="_blank"
+          target="_blank" title="formulaire de contact - nouvelle fenêtre"
           >formulaire de contact</a
         >
       </li>
@@ -219,7 +69,7 @@ import PageMeta from "../../components/PageMeta";
         la
         <a
           href="https://www.defenseurdesdroits.fr/fr/saisir/delegues"
-          target="_blank"
+          target="_blank" title="liste des délégués de votre région - nouvelle fenêtre"
           >liste des délégués de votre région</a
         >
         avec leurs informations de contact directs
@@ -231,7 +81,7 @@ import PageMeta from "../../components/PageMeta";
       <li>
         une adresse postale dans l’<a
           href="https://lannuaire.service-public.fr/autorites-independantes/1867f065-c823-4362-8d0f-8ca6b011a10f"
-          tarrget="_blank"
+          tarrget="_blank" title="annuaire de l’administration - nouvelle fenêtre"
           >annuaire de l’administration</a
         >
       </li>
