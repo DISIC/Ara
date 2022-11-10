daff427686356b324a09b95c1a7cf93804777b66
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2022-11-10 18:10:13 +0100

    update a11y statement

--- confiture-web-app/src/pages/misc/AccessibilityPage.vue
+++ confiture-web-app/src/pages/misc/AccessibilityPage.vue
@@ -13,8 +13,7 @@ import PageMeta from "../../components/PageMeta";
   <section class="fr-my-0 fr-mx-auto content">
     <h1>Déclaration d’accessibilité</h1>
     <p>
-      La
-      <strong>direction interministérielle du numérique (DINUM)</strong>
+      La <strong>direction interministérielle du numérique (DINUM)</strong>
       s’engage à rendre ses sites internet, intranet, extranet et ses progiciels
       accessibles (et ses applications mobiles et mobilier urbain numérique)
       conformément à l’article 47 de la loi n°2005-102 du 11 février 2005.
@@ -29,22 +28,10 @@ import PageMeta from "../../components/PageMeta";
     <p>Aucun audit n’a été encore effectué.<br/>
     Ara.numerique.gouv.fr est non conforme avec le référentiel général
       d’amélioration de l’accessibilité (RGAA).</p>
-    <p>Un audit de conformité est bien planifié et le résultat sera publié avant la fin de la version bêta</p>
+    <p>Un audit de conformité est bien planifié et l’état de conformité sera mis à jour avant la fin de la version bêta.</p>
 
     <h2 id="contact">Retour d’information et contact</h2>
-    <p>
-      La direction interministérielle du numérique (DINUM) s’engage à prendre
-      les moyens nécessaires afin de donner accès, dans un délai raisonnable,
-      aux informations et fonctionnalités recherchées par la personne
-      handicapée, que le contenu fasse l’objet d’une dérogation ou non.
-    </p>
-    <p>
-      La DINUM invite les personnes qui rencontreraient des difficultés à la
-      contacter par courriel à communication.dinum@modernisation.gouv.fr afin
-      qu’une assistance puisse être apportée (alternative accessible,
-      information et contenu donnés sous une autre forme).
-    </p>
-
+    <p>Si vous n’arrivez pas à accéder à un contenu ou à un service, vous pouvez nous contacter par courriel à l’adresse <strong>contact@design.numerique.gouv.fr</strong> pour être orienté vers une alternative accessible ou obtenir le contenu sous une autre forme.</p>
 
     <h2 id="recours">Voies de recours</h2>
     <p>
@@ -61,14 +48,14 @@ import PageMeta from "../../components/PageMeta";
         un
         <a
           href="https://www.defenseurdesdroits.fr/nous-contacter"
-          target="_blank" title="formulaire de contact - nouvelle fenêtre"
+          target="_blank" el="noreferrer noopener" title="formulaire de contact - nouvelle fenêtre"
           >formulaire de contact</a
         >
       </li>
       <li>
         la
         <a
-          href="https://www.defenseurdesdroits.fr/fr/saisir/delegues"
+          href="https://www.defenseurdesdroits.fr/fr/saisir/delegues" el="noreferrer noopener" 
           target="_blank" title="liste des délégués de votre région - nouvelle fenêtre"
           >liste des délégués de votre région</a
         >
@@ -80,8 +67,8 @@ import PageMeta from "../../components/PageMeta";
       </li>
       <li>
         une adresse postale dans l’<a
-          href="https://lannuaire.service-public.fr/autorites-independantes/1867f065-c823-4362-8d0f-8ca6b011a10f"
-          tarrget="_blank" title="annuaire de l’administration - nouvelle fenêtre"
+          href="https://lannuaire.service-public.fr/autorites-independantes/1867f065-c823-4362-8d0f-8ca6b011a10f" rel="noreferrer noopener" 
+          target="_blank" title="annuaire de l’administration - nouvelle fenêtre"
           >annuaire de l’administration</a
         >
       </li>
