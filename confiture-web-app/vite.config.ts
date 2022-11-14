fbd2218dd217d031029149618636812fb8c173bf
Adrien Boutigny <adrien@slash-tmp.dev>
2022-11-14 15:53:11 +0100

    51 implémenter analytics + modale cookies (#178)
    * chore: add matomo script
    
    * enable matomo only in production

--- confiture-web-app/vite.config.ts
+++ confiture-web-app/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["**/*.docx"],
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteEjsPlugin((config) => {
+      return {
+        VITE_MATOMO_ENABLE: config.env.VITE_MATOMO_ENABLE,
+      };
+    }),
+  ],
   server: {
     proxy: {
       "/api": "http://localhost:4000",
