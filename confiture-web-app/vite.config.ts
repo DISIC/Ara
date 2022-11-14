b496378b058619fe23c79f341d9fdbd33be3f18f
Adrien Boutigny <adrien@slash-tmp.dev>
2022-11-14 15:35:42 +0100

    chore: add matomo script

--- confiture-web-app/vite.config.ts
+++ confiture-web-app/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["**/*.docx"],
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteEjsPlugin((config) => {
+      return {
+        VITE_MATOMO_BASE_URL: config.env.VITE_MATOMO_BASE_URL,
+        VITE_MATOMO_SITE_ID: config.env.VITE_MATOMO_SITE_ID,
+      };
+    }),
+  ],
   server: {
     proxy: {
       "/api": "http://localhost:4000",
