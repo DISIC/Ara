4085a3c0c3105fe060be79bc7c84476c7ea2dcda
Adrien Boutigny <adrien@slash-tmp.dev>
2022-11-30 21:59:40 +0100

    move configuration schema to its own file

--- confiture-rest-api/src/app.module.ts
+++ confiture-rest-api/src/app.module.ts
@@ -1,36 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
 import { FeedbackModule } from './feedback/feedback.module';
 import { AuditsModule } from './audits/audits.module';
 import { HealthCheckController } from './health-check.controller';
+import { configValidationSchema } from './config-validation-schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validationSchema: Joi.object({
-        DATABASE_URL: Joi.string()
-          .uri({
-            scheme: 'postgres',
-          })
-          .required(),
-        MAILER_USER: Joi.string().email().required(),
-        MAILER_PASSWORD: Joi.string().required(),
-        MAILER_SMTP_HOST: Joi.string().hostname().required(),
-        MAILER_SMTP_PORT: Joi.number().integer().positive().required(),
-        MAILER_SMTP_SECURE: Joi.boolean().required(),
-        FRONT_BASE_URL: Joi.string()
-          .uri({ scheme: ['http', 'https'] })
-          .default('http://localhost:3000'),
-        AIRTABLE_BASE_ID: Joi.string().required(),
-        AIRTABLE_TABLE_ID: Joi.string().required(),
-        AIRTABLE_ACCESS_TOKEN: Joi.string().required(),
-      }),
+      validationSchema: configValidationSchema,
     }),
     FeedbackModule,
     AuditsModule,
   ],
-  controllers: [HealthCheckController]
+  controllers: [HealthCheckController],
 })
 export class AppModule {}
