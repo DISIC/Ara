19a94da90afcf223d062de9738e42257c8a9bfc4
Adrien Boutigny <adrien@slash-tmp.dev>
2022-12-09 11:27:36 +0100

    parse error description and recommendation markdown into html

---
+++ confiture-web-app/src/components/MarkdownRenderer.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { marked } from "marked";
+import { computed } from "vue";
+
+const props = defineProps<{
+  markdown: string;
+}>();
+
+const html = computed(() => marked.parse(props.markdown));
+</script>
+
+<template>
+  <div v-html="html"></div>
+</template>
