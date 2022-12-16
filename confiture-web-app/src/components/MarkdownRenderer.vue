2389c43c7f58dd8c1d6c7794d7717bf00441181a
Adrien Boutigny <adrien@slash-tmp.dev>
2022-12-16 11:32:01 +0100

    Sanitise le HTML rendu par le MarkdownRenderer

--- confiture-web-app/src/components/MarkdownRenderer.vue
+++ confiture-web-app/src/components/MarkdownRenderer.vue
@@ -1,12 +1,17 @@
 <script lang="ts" setup>
 import { marked } from "marked";
 import { computed } from "vue";
+import DOMPurify from "dompurify";
 
 const props = defineProps<{
   markdown: string;
 }>();
 
-const html = computed(() => marked.parse(props.markdown));
+const html = computed(() => {
+  const rawHtml = marked.parse(props.markdown);
+  const sanitizedHtml = DOMPurify.sanitize(rawHtml);
+  return sanitizedHtml;
+});
 </script>
 
 <template>
