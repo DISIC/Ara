e1c34bf36ce23cc4e9a841c07ac18d6f37758f1f
Adrien Boutigny <adrien@slash-tmp.dev>
2022-12-16 14:35:02 +0100

    remove frontend unused deps

--- confiture-web-app/vite.config.ts
+++ confiture-web-app/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import sentryVitePlugin from "@sentry/vite-plugin";
 
@@ -32,10 +32,5 @@ export default defineConfig({
     proxy: {
       "/api": "http://localhost:4000",
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["src/setupTests.ts"],
   },
 });
