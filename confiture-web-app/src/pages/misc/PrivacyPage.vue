46dd9d728f3f0d1a85ecfb5b8d8b2e011180d18f
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2023-01-06 17:35:25 +0100

    add privacy content

--- confiture-web-app/src/pages/misc/PrivacyPage.vue
+++ confiture-web-app/src/pages/misc/PrivacyPage.vue
@@ -12,6 +12,24 @@ import PageMeta from "../../components/PageMeta";
   />
   <section class="fr-my-0 fr-mx-auto content">
     <h1>Politique de confidentialité des données personnelles</h1>
+    <h2>Traitement des données</h2>
+    <p>La Direction interministérielle du numérique s’engage à ce que le traitement des données personnelles effectué sur ara.numerique.gouv.fr soient conformes au règlement général sur la protection des données (RGPD) et à la loi Informatique et Libertés.</p>
+
+    <h2>Objet du traitement</h2>
+    <p>Ara est un outil d’audit d’accessibilité numérique. Un audit est réalisé par une personne experte qui doit saisir son nom et son adresse électronique. Au moment de la création de l'audit, l’expert ou experte choisit si ses nom et adresse électronique peuvent être publiés ou non.</p>
+    <p>Ces données personnelles sont utilisées uniquement pour envoyer par courriel l’URL de l’audit en cours de réalisation.</p>
+    <p>Lors de la suppression de l’audit, les données de l’audit et les données personnelles sont supprimées.</p>
+    <h2>Vos droits sur vos données</h2>
+    <p>Vous pouvez exercer vos droits ou obtenir des informations concernant le traitement de vos données personnelles par courriel à l'adresse suivante : <strong>contact@design.numerique.gouv.fr</strong>.</p>
+    <p>Si la réponse apportée ne vous convient pas, vous pouvez alors saisir le délégué à la protection des données des services du Premier ministre.</p>
+    <ul>
+    <li>par courriel à l'adresse suivante : dpd@pm.gouv.fr ;</li>
+    <li>par courrier à l'adresse suivante :<br/>
+    Service du Premier ministre<br/>
+    À l'attention du délégué à la protection des données (DPD)<br/>
+    56 rue de Varenne<br/>75700 Paris</li>
+    </ul>
+
 
   </section>
 </template>
