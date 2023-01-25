8c726a9a2cb11332d31adf6962083bbd53533088
Adrien Boutigny <adrien@slash-tmp.dev>
2023-01-25 11:58:45 +0100

    remove unused files

--- confiture-rest-api/scripts/s3-client.ts
+++
@@ -1,9 +0,0 @@
-// Create service client module using ES6 syntax.
-import { S3Client } from '@aws-sdk/client-s3';
-
-const s3Client = new S3Client({
-  region: 'gra',
-  endpoint: 'https://s3.gra.io.cloud.ovh.net/',
-});
-
-export default s3Client;
