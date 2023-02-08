e7840d2d852e643f895911047d842f98ed2e470c
Quentin Bellanger <hello@quentin-bellanger.com>
2023-02-08 11:55:44 +0100

    fix: update chart colors when theme changes with theme modal

--- confiture-web-app/src/composables/useChartColorsUpdate.ts
+++ confiture-web-app/src/composables/useChartColorsUpdate.ts
@@ -35,11 +35,22 @@ export function useChartColorsUpdate(
     chart?.update();
   }
 
+  // Update chart colors on `data-fr-theme` attribute change
+  const observer = new MutationObserver((mutationList) => {
+    if (!mutationList.some((m) => m.attributeName === "data-fr-theme")) {
+      return;
+    }
+
+    updateChartColors();
+  });
+
   onMounted(() => {
     mediaQueryList.addEventListener("change", updateChartColors);
+    observer.observe(document.documentElement, { attributes: true });
   });
 
   onUnmounted(() => {
     mediaQueryList.removeEventListener("change", updateChartColors);
+    observer.disconnect();
   });
 }
