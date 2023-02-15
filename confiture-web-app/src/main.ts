1e38a37d194f4ce75603dd981ce74871abd6a50d
Adrien Boutigny <adrien@slash-tmp.dev>
2023-02-15 12:20:15 +0100

    enable sentry replays on error

--- confiture-web-app/src/main.ts
+++ confiture-web-app/src/main.ts
@@ -57,11 +57,21 @@ if (import.meta.env.VITE_SENTRY_DSN) {
         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
         tracePropagationTargets: [window.location.hostname, /^\//],
       }),
+      new Sentry.Replay({
+        // anonymization settings
+        maskAllText: true,
+        maskAllInputs: true,
+        blockAllMedia: true,
+      }),
     ],
 
     // Set to a value between 0 and 1.0 to enable performance monitoring
     // 0.5 means half the events will be transferred
     tracesSampleRate: 0,
+
+    // When an error happen save a replay of what happened similar to birdeatsbug
+    // See https://docs.sentry.io/platforms/javascript/guides/react/session-replay/#replay-captures-on-errors-only
+    replaysOnErrorSampleRate: 1,
 
     /*
       Differentiate between the various environments of the app
