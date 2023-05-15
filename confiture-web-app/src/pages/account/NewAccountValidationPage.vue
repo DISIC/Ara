8d64d972351b7477be85d19801a6987b57971f6e
Adrien Boutigny <adrien@slash-tmp.dev>
2023-05-15 18:23:36 +0200

    add login page

---
+++ confiture-web-app/src/pages/account/NewAccountValidationPage.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import jwt_decode from "jwt-decode";
+
+import { useAccountStore } from "../../store/account";
+import router from "../../router";
+import { AccountVerificationJwtPayload } from "../../types";
+
+const route = useRoute();
+const store = useAccountStore();
+const tokenIsInvalid = ref(false);
+
+onMounted(async () => {
+  const verificationToken = route.query.token;
+
+  console.log(verificationToken);
+
+  if (typeof verificationToken !== "string") {
+    tokenIsInvalid.value = true;
+    return;
+  }
+
+  store
+    .verifyAccountCreation(verificationToken)
+    .then(() => {
+      const payload = jwt_decode(
+        verificationToken
+      ) as AccountVerificationJwtPayload;
+
+      router.push({ name: "login", state: { email: payload.sub } });
+    })
+    .catch((err) => {
+      tokenIsInvalid.value = true;
+    });
+});
+</script>
+
+<template>
+  <p>Validation en cours...</p>
+
+  <p v-if="tokenIsInvalid">Gaaaaah ça marche pô :'(</p>
+</template>
--- confiture-web-app/src/types/index.ts
+++ confiture-web-app/src/types/index.ts
@@ -1,2 +1,15 @@
 export * from "./types";
 export * from "./report";
+
+export interface AccountVerificationJwtPayload {
+  /** User email */
+  sub: string;
+  /**
+   * JWT ID
+   *
+   * At any time, only one verification token should be considered valid. The
+   * `jti` property will be compared with the one stored in the DB to make sure
+   * this is the latest verification token generated.
+   */
+  jti: string;
+}
