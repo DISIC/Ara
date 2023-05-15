d3470107f5c84c16e786b8da14e1b6ad066c8ef6
Adrien Boutigny <adrien@slash-tmp.dev>
2023-05-15 14:32:29 +0200

    only allow one valid verification link at a time

---
+++ confiture-rest-api/src/auth/jwt-payloads.ts
@@ -0,0 +1,17 @@
+export interface AuthenticationJwtPayload {
+  /** User email */
+  sub: string;
+}
+
+export interface AccountVerificationJwtPayload {
+  /** User email */
+  sub: string;
+  /**
+   * JWT ID
+   *
+   * At any time, only one verification token should be considered valid. The
+   * `jti` property will be compared with the one stored in the DB to make sure
+   * this is the latest verification token generated.
+   */
+  jti: string;
+}
