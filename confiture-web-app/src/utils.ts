645e8f4d6fbcbf8b7bf1b190fc7f6ec40dfc28ac
Adrien Boutigny <adrien@slash-tmp.dev>
2023-10-12 16:36:11 +0200

    validator new account form before submit

--- confiture-web-app/src/utils.ts
+++ confiture-web-app/src/utils.ts
@@ -185,3 +185,10 @@ export async function captureWithPayloads(
 // TODO: use everywhere
 export const pluralize = (singular: string, plural: string, count: number) =>
   count === 1 ? singular : plural;
+
+const EMAIL_REGEX =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export function validateEmail(s: string): boolean {
+  return !!s.match(EMAIL_REGEX);
+}
