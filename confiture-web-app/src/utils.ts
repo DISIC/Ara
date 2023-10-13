9607397f8c10f53db01e4fc8a171ab9fcabcb816
Adrien Boutigny <adrien@slash-tmp.dev>
2023-10-13 11:08:10 +0200

    add source for email regex

--- confiture-web-app/src/utils.ts
+++ confiture-web-app/src/utils.ts
@@ -186,6 +186,8 @@ export async function captureWithPayloads(
 export const pluralize = (singular: string, plural: string, count: number) =>
   count === 1 ? singular : plural;
 
+// From https://emailregex.com/
+// regexr.com/7lkj4
 const EMAIL_REGEX =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
