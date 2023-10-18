ca5ed8c415f94c26757cde7a017dd486549791d0
Quentin Bellanger <hello@quentin-bellanger.com>
2023-10-18 15:58:02 +0200

    reset toggle password state when changing step

--- confiture-web-app/src/components/account/new-acccount/NewAccountForm.vue
+++ confiture-web-app/src/components/account/new-acccount/NewAccountForm.vue
@@ -14,6 +14,7 @@ const emit = defineEmits<{
 
 const userEmail = ref("");
 const userPassword = ref("");
+const togglePasswordRef = ref<HTMLInputElement>();
 const userEmailField = ref<InstanceType<typeof DsfrField>>();
 const userEmailError = ref<string>();
 
@@ -27,6 +28,10 @@ async function handleSubmit() {
     .createAccount(userEmail.value, userPassword.value)
     .then(() => {
       emit("submit", { username: userEmail.value });
+
+      if (togglePasswordRef.value) {
+        togglePasswordRef.value.value = "false";
+      }
     })
     .catch(async (err) => {
       if (err instanceof HTTPError) {
@@ -121,6 +126,7 @@ function fillFields() {
         >
           <input
             id="user-password-show"
+            ref="togglePasswordRef"
             aria-label="Afficher le mot de passe"
             type="checkbox"
             aria-describedby="user-password-show-messages"
