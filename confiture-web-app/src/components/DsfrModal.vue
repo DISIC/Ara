3bee84997dfbc614355d0763a2cd31bc3b6ae474
Adrien Boutigny <adrien@slash-tmp.dev>
2023-12-11 11:39:25 +0100

    fix manually opened modals not working

--- confiture-web-app/src/components/DsfrModal.vue
+++ confiture-web-app/src/components/DsfrModal.vue
@@ -7,6 +7,10 @@ export default {
 <script lang="ts" setup>
 import { ref } from "vue";
 
+defineProps<{
+  id: string;
+}>();
+
 const emit = defineEmits<{
   (e: "closed"): void;
 }>();
@@ -16,9 +20,13 @@ const modal = ref<HTMLDialogElement>();
 const triggerElement = ref<HTMLElement>();
 
 function show() {
+  console.log("DSFR MODAL");
+
   if (document.activeElement) {
     triggerElement.value = document.activeElement as HTMLElement;
   }
+
+  console.log(dsfr(modal.value).modal);
 
   dsfr(modal.value).modal.disclose();
 }
@@ -27,7 +35,19 @@ function hide() {
   dsfr(modal.value).modal.conceal();
 }
 
+const isOpened = ref(false);
+
 function onConceal() {
+  /* 
+  FIXME: For some reason, the DSFR modal emits the `dsfr.conceal` event as
+  soon as the page loads. We want to ignore this one event fire so we track if
+  the modal is *actually* opened before firing our own event.
+  */
+  if (!isOpened.value) {
+    return;
+  }
+
+  isOpened.value = false;
   setTimeout(() => {
     if (triggerElement.value && triggerElement.value.isConnected) {
       triggerElement.value.focus();
@@ -36,17 +56,30 @@ function onConceal() {
   });
 }
 
+function onDisclose() {
+  isOpened.value = true;
+}
+
 defineExpose({ show, hide });
 </script>
 
 <template>
   <Teleport to="body">
+    <!--
+    FIXME: For some reason, in v1.10 of the DSFR, a modal MUST have an activation
+    button. Otherwise the javascript API to manually open the modal won't work.
+
+    See: https://github.com/GouvernementFR/dsfr/issues/728
+    -->
+    <button hidden data-fr-opened="false" :aria-controls="id"></button>
+
     <dialog
+      :id="id"
       ref="modal"
       role="dialog"
       class="fr-modal"
       v-bind="$attrs"
-      v-on="{ 'dsfr.conceal': onConceal }"
+      v-on="{ 'dsfr.conceal': onConceal, 'dsfr.disclose': onDisclose }"
     >
       <slot />
     </dialog>
