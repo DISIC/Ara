37e6bc8e7cd37c8a0e7d58dfd421dc9db152e141
Adrien Boutigny <adrien@slash-tmp.dev>
2023-12-14 15:03:15 +0100

    remove unused dependencies

--- confiture-web-app/vite.config.ts
+++ confiture-web-app/vite.config.ts
@@ -11,7 +11,7 @@ const uploadSourceMapsToSentry =
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true, // Source map generation must be turned on
+    sourcemap: true // Source map generation must be turned on
   },
   assetsInclude: ["**/*.docx", "**/*.md"],
   plugins: [
@@ -23,15 +23,15 @@ export default defineConfig({
             org: process.env.SENTRY_ORG,
             project: process.env.SENTRY_PROJECT,
             authToken: process.env.SENTRY_AUTH_TOKEN,
-            release: process.env.VITE_SENTRY_RELEASE,
-          }),
+            release: process.env.VITE_SENTRY_RELEASE
+          })
         ]
-      : []),
+      : [])
   ],
   server: {
     proxy: {
-      "/api": "http://localhost:4000",
+      "/api": "http://localhost:4000"
     },
-    port: 3000,
-  },
+    port: 3000
+  }
 });
