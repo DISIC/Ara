14626df8c2d00c3bd919c681b4460a05881f0c6f
Adrien Boutigny <adrien@slash-tmp.dev>
2023-12-15 15:45:34 +0100

    add placeholder account spec file

---
+++ cypress/e2e/account.cy.ts
@@ -0,0 +1,12 @@
+describe("Accounts", () => {
+  it.skip("User can create a new account", () => {});
+  it.skip("User can sign in with their account", () => {});
+  it.skip("User can update their user profile", () => {});
+  it.skip("User can update their password", () => {});
+  it.skip("User can reset their password", () => {});
+  it.skip("User can update their email address", () => {});
+  it.skip("User can delete their account from Ara", () => {});
+
+  it.skip("User can access all their audits", () => {});
+  it.skip("User profile info are prefilled on new audits", () => {});
+});
