99df922b81bf4f996031e970e93a0802f8001687
Quentin Bellanger <hello@quentin-bellanger.com>
2024-03-14 11:31:09 +0100

    design deliverable header

---
+++ confiture-web-app/src/components/layout/DeliverableHeader.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header id="header" role="banner" class="fr-header">
+    <div class="fr-header__body">
+      <div class="fr-container">
+        <div class="fr-header__body-row">
+          <div class="fr-header__brand">
+            <div class="fr-header__brand-top">
+              <div class="fr-header__logo">
+                <p class="fr-logo">
+                  République
+                  <br />Française
+                </p>
+              </div>
+            </div>
+            <div class="fr-header__service">
+              <p class="fr-header__service-title">
+                Ara
+                <span
+                  class="fr-badge fr-badge--sm fr-badge--info fr-badge--no-icon"
+                  >BÊTA</span
+                >
+              </p>
+              <p class="fr-header__service-tagline">
+                Réalisez vos audits d’accessibilité numérique
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.user-dropdown {
+  text-align: initial !important;
+}
+</style>
