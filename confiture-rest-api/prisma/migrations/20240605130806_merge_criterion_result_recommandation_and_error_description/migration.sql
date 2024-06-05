4564eacde1bfc1f9f9d79f3d6c230c9ae1f774bd
Adrien Boutigny <adrien@slash-tmp.dev>
2024-06-05 16:09:09 +0200

    fix migration

--- confiture-rest-api/prisma/migrations/20240605130806_merge_criterion_result_recommandation_and_error_description/migration.sql
+++ confiture-rest-api/prisma/migrations/20240605130806_merge_criterion_result_recommandation_and_error_description/migration.sql
@@ -2,13 +2,16 @@
 UPDATE
   "CriterionResult"
 SET
-  "errorDescription" = "errorDescription" || E'\n\n---\n\n' || "recommandation"
+  "errorDescription" = 
+    CASE
+      WHEN "errorDescription" IS NULL OR LENGTH(BTRIM("errorDescription", E' \t\r\n')) = 0
+        THEN BTRIM("recommandation", E' \t\r\n')
+      ELSE
+        BTRIM(COALESCE("errorDescription", ''), E' \t\r\n') || E'\n\n---\n\n' || BTRIM("recommandation", E' \t\r\n')
+    END
 WHERE
   "recommandation" IS NOT NULL
-  AND LENGTH(
-    BTRIM(
-      "recommandation",
-      ' \t\')) > 0;
+  AND LENGTH(BTRIM("recommandation", E' \t\r\n')) > 0;
 
 /*
   Warnings:
