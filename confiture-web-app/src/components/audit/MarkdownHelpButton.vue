dcd525e4f5891badcbb2757f9cb8a75e59a88210
Yaacov <yaacov@goodimpact.studio>
2024-06-20 10:07:57 +0200

    Fix Mardown helper style (link instead of button)

--- confiture-web-app/src/components/audit/MarkdownHelpButton.vue
+++ confiture-web-app/src/components/audit/MarkdownHelpButton.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="markdown-wrapper">
-    <p class="fr-text--sm fr-m-0">
-      Markdown pris en compte
-      <RouterLink
-        class="fr-btn fr-btn--tertiary-no-outline fr-btn--sm"
-        href="/syntaxe-markdown"
-        target="_blank"
-        title="Voir la syntaxe Markdown - nouvelle fenêtre"
-        to="{ name: 'markdown-syntax'}"
-      >
-        Voir la syntaxe<span class="sr-only"> Markdown</span>
-      </RouterLink>
-    </p>
-  </div>
+  <p class="markdown-wrapper fr-text--sm fr-m-0">
+    <span class="markdown-wrapper__text">Markdown pris en compte</span>
+    <br class="fr-sr-only" />
+    <RouterLink
+      class="fr-link fr-text--sm markdown-wrapper__link"
+      href="/syntaxe-markdown"
+      target="_blank"
+      title="Voir la syntaxe Markdown - nouvelle fenêtre"
+      to="{ name: 'markdown-syntax'}"
+      >Voir la syntaxe<span class="sr-only"> Markdown</span>
+    </RouterLink>
+  </p>
 </template>
 
 <style scoped>
