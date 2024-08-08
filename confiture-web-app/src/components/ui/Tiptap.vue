64831245e072ef9260c14a927ac5d8a38d5112ec
Yaacov <yaacov@goodimpact.studio>
2024-08-09 00:53:56 +0200

    Add Tiptap and a first integration in NotesModal
    * Should handle existing markdown content
    * The editor is configured with extensions:
      * all what's in StarterKit
      * code block lowlight
      * highlight
      * link
      * task
      * typography (with french double quotes)
    * Some styles have been added to fit DSFR styling
    
    Still work in progress, but a nice start!
    
    To do:
    * image uploading and placement inside editor
    * menu buttons
    * accessibility tests and improvements

---
+++ confiture-web-app/src/components/ui/Tiptap.vue
@@ -0,0 +1,75 @@
+<script lang="ts" setup>
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import Heading from "@tiptap/extension-heading";
+import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import Typography from "@tiptap/extension-typography";
+import { useEditor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+
+import css from "highlight.js/lib/languages/css";
+import js from "highlight.js/lib/languages/javascript";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
+
+import { Markdown } from "tiptap-markdown";
+
+// load common languages
+import { common, createLowlight } from "lowlight";
+
+// create a lowlight instance
+const lowlight = createLowlight(common);
+
+// you can also register languages
+lowlight.register("html", html);
+lowlight.register("css", css);
+lowlight.register("js", js);
+lowlight.register("ts", ts);
+
+const props = defineProps(["content"]);
+const emit = defineEmits(["update:content"]);
+//JSON.stringify(document.querySelector(".tiptap").editor.getJSON())
+
+function getContent() {
+  let jsonContent;
+  try {
+    jsonContent = JSON.parse(props.content);
+  } catch (e) {
+    return "";
+  }
+  return jsonContent;
+}
+
+const editor = useEditor({
+  content: getContent(),
+  extensions: [
+    CodeBlockLowlight.configure({ lowlight }),
+    Highlight,
+    Heading.configure({
+      levels: [2, 3, 4, 5, 6]
+    }),
+    Link,
+    Markdown,
+    StarterKit.configure({
+      codeBlock: false,
+      heading: false
+    }),
+    TaskItem,
+    TaskList,
+    Typography.configure({
+      openDoubleQuote: "« ",
+      closeDoubleQuote: " »"
+    })
+  ],
+  onUpdate({ editor }) {
+    // The content has changed.
+    emit("update:content", JSON.stringify(editor.getJSON()));
+  }
+});
+</script>
+
+<template>
+  <editor-content :editor="editor" />
+</template>
