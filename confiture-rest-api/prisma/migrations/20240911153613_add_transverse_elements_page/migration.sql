1561a355386abacfc4a5d859e20bfd18adacd6be
Quentin Bellanger <hello@quentin-bellanger.com>
2024-10-11 11:30:02 +0200

    migration to add transverse page to audits

--- confiture-rest-api/prisma/migrations/20240911153613_add_transverse_elements_page/migration.sql
+++ confiture-rest-api/prisma/migrations/20240911153613_add_transverse_elements_page/migration.sql
@@ -1,12 +1,22 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[transverseElementsPageId]` on the table `Audit` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `transverseElementsPageId` to the `Audit` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "Audit" ADD COLUMN     "transverseElementsPageId" INTEGER NOT NULL;
+ALTER TABLE "Audit" ADD COLUMN     "transverseElementsPageId" INTEGER;
+
+-- Create one transverse elements page for each existing audit and link it
+DO $$
+DECLARE temprow RECORD;
+DECLARE transversePageId "AuditedPage"."id"%TYPE;
+BEGIN FOR temprow IN
+        SELECT * FROM "Audit" WHERE "transverseElementsPageId" IS NULL
+    LOOP
+      -- Crée la page élément transverse
+    INSERT INTO "AuditedPage"("name", "url", "order") VALUES ('Éléments transverses (optionnel)', '', 0) RETURNING "id" INTO transversePageId;
+        -- Lie la page à l'audit
+    UPDATE "Audit" SET "transverseElementsPageId" = transversePageId WHERE "id" = "temprow"."id";
+  END LOOP;
+END; $$
+
+-- Make transverseElementsPageId not nullable
+ALTER TABLE "Audit" ALTER COLUMN "transverseElementsPageId" SET NOT NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Audit_transverseElementsPageId_key" ON "Audit"("transverseElementsPageId");
