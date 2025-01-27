7dab077d6cb226dce950a03a6c7977fe11693cad
Yaacov <yaacov@goodimpact.studio>
2025-01-27 18:08:53 +0100

    Prisma: better query logs (DEBUG="prisma:query")
    Use env variable DEBUG="prisma:query"
    
    Note: queries using an existing connection are not properly logged (without var replacement)
    This could be improved when upgrading primsa

--- confiture-rest-api/src/prisma.service.ts
+++ confiture-rest-api/src/prisma.service.ts
@@ -1,10 +1,47 @@
 import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<
+    Prisma.PrismaClientOptions,
+    "query" | "info" | "warn" | "error"
+  >
+  implements OnModuleInit
+{
+  constructor() {
+    const debugQuery = process.env.DEBUG === "prisma:query";
+    let logObject = {};
+    if (debugQuery) {
+      // Note: ideally we would use `emit: "event"` instead of `emit: "stdout"`
+      // to avoid double logging, but Prisma's query logging is connection-scoped
+      // rather than query-scoped, so queries using an existing connection
+      // would not appear in logs
+      // TODO: improve when upgrading Prisma
+      logObject = {
+        log: [
+          {
+            emit: "stdout",
+            level: "query"
+          }
+        ]
+      };
+    }
+    super(logObject);
+  }
+
   async onModuleInit() {
     await this.$connect();
+    this.$on("query", (query: Prisma.QueryEvent) => {
+      let q = query.query;
+      JSON.parse(query.params).forEach((e, i) => {
+        q = q.replace(`$${i + 1}`, `'${e}'`);
+      });
+      console.log("=======================================");
+      console.log("--- Prisma Query (with $n replaced) ---");
+      console.log(q);
+      console.log("Duration: " + query.duration + "ms\n");
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
