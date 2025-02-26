771f25ffe0f9aab6f0b68057c6b164f63546787d
Quentin Bellanger <hello@quentin-bellanger.>
2025-02-26 17:13:40 +0100

    make sure connected user can see all audit info from other ppl's audits

--- confiture-web-app/src/composables/useIsConnected.ts
+++ confiture-web-app/src/composables/useIsConnected.ts
@@ -1,9 +1,20 @@
 import { computed } from "vue";
 
-import { useAccountStore } from "../store";
+import { useAccountStore, useAuditStore } from "../store";
 
+/**
+ * Make sure user is connected and owns the current audit (if any)
+ */
 export function useIsConnected() {
-  const store = useAccountStore();
+  const accountStore = useAccountStore();
+  const auditStore = useAuditStore();
 
-  return computed(() => store.account?.email);
+  return computed(() => {
+    if (auditStore.currentAudit) {
+      return (
+        auditStore.currentAudit?.auditorEmail === accountStore.account?.email
+      );
+    }
+    return accountStore.account?.email;
+  });
 }
