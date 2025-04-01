c3d8a597327144a66542a904b720929165b5b388
Yaacov <yaacov@goodimpact.studio>
2025-04-01 22:42:27 +0200

    feat(cypress/audit): add test for tabs (prev/next)
    Test is: "User can go back to previous/next tab with navigator back and previous buttons"

--- cypress/e2e/audit.cy.ts
+++ cypress/e2e/audit.cy.ts
@@ -128,6 +128,36 @@ describe("Audit", () => {
     });
   });
 
+  it("User can go back to previous/next tab with navigator back and previous buttons", () => {
+    cy.createTestAudit().then(({ editId }) => {
+      const slug = slugify(auditJson.pages[2].name);
+      const nextSlug = slugify(auditJson.pages[3].name);
+
+      cy.visit(`http://localhost:3000/audits/${editId}/generation/${slug}`);
+
+      cy.get(`.tabs button[aria-selected="true"]`)
+        .should("exist")
+        .parent().next().find("button").click();
+      cy.get(`.tabs button[aria-selected="true"]`)
+        .invoke('attr', 'data-slug')
+        .should('eq', `${nextSlug}`);
+
+      cy.go('back');
+
+      cy.get(`.tabs button[data-slug="${slug}"]`)
+        .should("exist")
+        .invoke('attr', 'aria-selected')
+        .should('eq', 'true');
+
+      cy.go('forward');
+
+      cy.get(`.tabs button[data-slug="${nextSlug}"]`)
+        .should("exist")
+        .invoke('attr', 'aria-selected')
+        .should('eq', 'true');
+    });
+  });
+
   it("User can edit pages", () => {
     cy.createTestAudit().then(({ editId }) => {
       cy.visit(`http://localhost:3000/audits/${editId}/parametres`);
