a126e89c61418636044a020e1876cad2d4f00ef1
Quentin Bellanger <hello@quentin-bellanger.com>
2025-04-02 12:10:53 +0200

    update multiple pages titles

--- confiture-web-app/src/pages/ChangelogPage.vue
+++ confiture-web-app/src/pages/ChangelogPage.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import changelogMarkdown from "../assets/CHANGELOG.md?raw";
+import PageMeta from "../components/PageMeta";
 import MarkdownRenderer from "../components/ui/MarkdownRenderer.vue";
 </script>
 
 <template>
+  <PageMeta title="Notes de versions" />
   <MarkdownRenderer :markdown="changelogMarkdown" />
 </template>
--- confiture-web-app/src/pages/RoadmapPage.vue
+++ confiture-web-app/src/pages/RoadmapPage.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import roadmapMarkdown from "../assets/ROADMAP.md?raw";
+import PageMeta from "../components/PageMeta";
 import MarkdownRenderer from "../components/ui/MarkdownRenderer.vue";
 </script>
 
 <template>
+  <PageMeta title="Feuille de route" />
   <MarkdownRenderer :markdown="roadmapMarkdown" />
 </template>
