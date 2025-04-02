836e1e598331f609b3a2aeec90d896357d4aca60
Adrien Boutigny <adrien@slash-tmp.dev>
2025-04-02 11:10:05 +0200

    update e2e test

--- cypress/e2e/audit.cy.ts
+++ cypress/e2e/audit.cy.ts
@@ -280,7 +280,7 @@ describe("Audit", () => {
         .clear()
         .type(statementJson.technologies);
 
-      cy.contains("button", "Valider les technologies").click();
+      cy.contains("button", "Ajouter les technologies").click();
 
       // Tools
       cy.contains("Web Developer Toolbar").click();
@@ -289,7 +289,7 @@ describe("Audit", () => {
         .clear()
         .type(statementJson.tools);
 
-      cy.contains("button", "Valider les outils").click();
+      cy.contains("button", "Ajouter les outils").click();
 
       // Environments
       cy.contains("Combinaison 1").click();
