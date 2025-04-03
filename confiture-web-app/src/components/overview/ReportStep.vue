c1266c9b673bbbcba979d62004617c75ef9f33c0
Quentin Bellanger <hello@quentin-bellanger.com>
2025-04-03 16:47:06 +0200

    update report step copy feature

--- confiture-web-app/src/components/overview/ReportStep.vue
+++ confiture-web-app/src/components/overview/ReportStep.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
+import router from "../../router";
 import { useResultsStore } from "../../store";
 import { Audit } from "../../types";
-import CopyBlock from "../ui/CopyBlock.vue";
+import CopyIcon from "../icons/CopyIcon.vue";
 import StepCard from "./StepCard.vue";
 
-defineProps<{
+const props = defineProps<{
   audit: Audit;
   headingLevel: "h2" | "h3";
 }>();
@@ -16,6 +17,29 @@ const resultsStore = useResultsStore();
 const auditIsReady = computed(() => {
   return resultsStore.auditProgress === 1;
 });
+
+const reportUrl = computed(
+  () =>
+    window.location.origin +
+    router.resolve({
+      name: "report",
+      params: { uniqueId: props.audit.consultUniqueId }
+    }).fullPath
+);
+
+const showCopyAlert = ref(false);
+const copyButtonRef = ref<HTMLButtonElement>();
+
+function copyReportUrl() {
+  navigator.clipboard.writeText(reportUrl.value).then(() => {
+    showCopyAlert.value = true;
+  });
+}
+
+function onReportAlertClose() {
+  copyButtonRef.value?.focus();
+  showCopyAlert.value = false;
+}
 </script>
 
 <template>
@@ -47,10 +71,7 @@ const auditIsReady = computed(() => {
       }}
     </p>
     <ul
-      :class="[
-        'fr-btns-group fr-btns-group--icon-left',
-        { 'fr-mb-3w': auditIsReady }
-      ]"
+      class="fr-btns-group fr-btns-group--inline-md fr-btns-group--icon-left report-step-actions"
     >
       <li>
         <RouterLink
@@ -66,21 +87,33 @@ const auditIsReady = computed(() => {
           <span class="fr-sr-only">(nouvelle fenêtre)</span>
         </RouterLink>
       </li>
+      <li v-if="auditIsReady">
+        <button
+          ref="copyButtonRef"
+          class="fr-btn fr-btn--secondary fr-mb-0"
+          @click="copyReportUrl"
+        >
+          <CopyIcon class="fr-mr-2v" />
+          Copier le lien de partage
+          <span class="fr-sr-only">du rapport</span>
+        </button>
+      </li>
     </ul>
 
-    <template v-if="auditIsReady">
-      <CopyBlock
-        class="fr-m-0 report-step-copy-block"
-        :to="{
-          name: 'report',
-          params: { uniqueId: audit.consultUniqueId }
-        }"
-        label="Lien de partage"
-        title="Lien de partage du rapport d’audit"
-        success-message="Le lien vers le rapport d’audit a bien été copié dans le presse-papier."
-        button-class="fr-btn--secondary"
-      />
-    </template>
+    <div role="alert" aria-live="polite" class="report-step-alert">
+      <div
+        v-if="showCopyAlert"
+        class="fr-alert fr-alert--success fr-alert--sm fr-mt-2w"
+      >
+        <p>
+          Le lien vers le rapport d’audit a bien été copié dans le
+          presse-papier.
+        </p>
+        <button class="fr-link--close fr-link" @click="onReportAlertClose">
+          Masquer le message
+        </button>
+      </div>
+    </div>
   </StepCard>
 </template>
 
@@ -93,5 +126,21 @@ const auditIsReady = computed(() => {
 .report-step-copy-block {
   grid-column: 1 / -1;
   grid-row: 4;
+}
+
+.report-step-actions {
+  grid-column: 1 / -1;
+
+  li:first-child {
+    width: 50%;
+  }
+
+  li > a {
+    width: 100%;
+  }
+}
+
+.report-step-alert {
+  grid-column: 1 / -1;
 }
 </style>
