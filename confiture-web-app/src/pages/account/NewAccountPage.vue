776cbc80060637465b88ed6902ab58d8c1148187
Yaacov <yaacov@goodimpact.studio>
2025-05-21 11:36:50 +0200

    fix(account): modify page title

--- confiture-web-app/src/pages/account/NewAccountPage.vue
+++ confiture-web-app/src/pages/account/NewAccountPage.vue
@@ -13,8 +13,8 @@ const userEmail = ref<string>();
 
 const pageTitles = [
   "Créer votre compte",
-  "Créer votre compte, consulter votre boite de réception",
-  "Créer votre compte, votre compte a été créé avec succès"
+  "Consulter votre boite de réception - Créer votre compte",
+  "Votre compte a été créé avec succès - Créer votre compte"
 ];
 
 const store = useAccountStore();
