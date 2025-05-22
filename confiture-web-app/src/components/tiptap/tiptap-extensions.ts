1726dd66f48385638251bc553140a41f200d6a2b
Quentin Bellanger <hello@quentin-bellanger.com>
2025-05-22 11:11:03 +0200

    add tiptap placeholder extension

--- confiture-web-app/src/components/tiptap/tiptap-extensions.ts
+++ confiture-web-app/src/components/tiptap/tiptap-extensions.ts
@@ -8,6 +8,7 @@ import DropCursor from "@tiptap/extension-dropcursor";
 import { Heading, type Level } from "@tiptap/extension-heading";
 import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
+import Placeholder from "@tiptap/extension-placeholder";
 import Typography from "@tiptap/extension-typography";
 import StarterKit from "@tiptap/starter-kit";
 import css from "highlight.js/lib/languages/css";
@@ -28,66 +29,82 @@ lowlight.register("css", css);
 lowlight.register("js", js);
 lowlight.register("ts", ts);
 
-export const tiptapExtensions: Extensions = [
-  Heading.extend({
-    // prevent all marks from being applied to headings
-    marks: "",
-    // Shift heading levels when typing markdown
-    // Example: "## Foobar" would render a `h5`
-    addInputRules() {
-      return this.options.levels.map((level) => {
-        return textblockTypeInputRule({
-          find: new RegExp(
-            `^(#{${Math.min(...this.options.levels) - 3},${level - 3}})\\s$`
-          ),
-          type: this.type,
-          getAttributes: {
-            level
-          }
+interface TiptapExtensionsParams {
+  placeholder?: string;
+}
+
+export function getTiptapExtensions(
+  params?: TiptapExtensionsParams
+): Extensions {
+  return [
+    Heading.extend({
+      // prevent all marks from being applied to headings
+      marks: "",
+      // Shift heading levels when typing markdown
+      // Example: "## Foobar" would render a `h5`
+      addInputRules() {
+        return this.options.levels.map((level) => {
+          return textblockTypeInputRule({
+            find: new RegExp(
+              `^(#{${Math.min(...this.options.levels) - 3},${level - 3}})\\s$`
+            ),
+            type: this.type,
+            getAttributes: {
+              level
+            }
+          });
         });
-      });
-    }
-  }).configure({
-    levels: displayedHeadings
-  }),
-  StarterKit.configure({
-    codeBlock: false,
-    dropcursor: false,
-    heading: false
-  }),
-  CodeBlockLowlight.configure({ lowlight, defaultLanguage: "html" }),
-  Link.extend({
-    addAttributes() {
-      return {
-        ...this.parent?.(),
-        class: {
-          default: null,
-          renderHTML: () => {
-            return { class: null }; // reset class when copy pasting for example
+      }
+    }).configure({
+      levels: displayedHeadings
+    }),
+    StarterKit.configure({
+      codeBlock: false,
+      dropcursor: false,
+      heading: false
+    }),
+    CodeBlockLowlight.configure({ lowlight, defaultLanguage: "html" }),
+    Link.extend({
+      addAttributes() {
+        return {
+          ...this.parent?.(),
+          class: {
+            default: null,
+            renderHTML: () => {
+              return { class: null }; // reset class when copy pasting for example
+            }
+          },
+          title: {
+            default: null,
+            renderHTML: (attributes) => {
+              return {
+                title: attributes.title
+              };
+            }
           }
-        },
-        title: {
-          default: null,
-          renderHTML: (attributes) => {
-            return {
-              title: attributes.title
-            };
-          }
-        }
-      };
-    },
-    renderHTML({ HTMLAttributes }) {
-      return ["a", mergeAttributes(HTMLAttributes), 0];
-    }
-  }).configure({
-    openOnClick: false,
-    defaultProtocol: "https"
-  }),
-  Typography.configure({
-    openDoubleQuote: "« ",
-    closeDoubleQuote: " »"
-  }),
-  Markdown.configure({ linkify: true }),
-  Image,
-  DropCursor.configure({ color: "var(--dsfr-outline)", width: 3 })
-];
+        };
+      },
+      renderHTML({ HTMLAttributes }) {
+        return ["a", mergeAttributes(HTMLAttributes), 0];
+      }
+    }).configure({
+      openOnClick: false,
+      defaultProtocol: "https"
+    }),
+    Typography.configure({
+      openDoubleQuote: "« ",
+      closeDoubleQuote: " »"
+    }),
+    Markdown.configure({ linkify: true }),
+    Image,
+    DropCursor.configure({ color: "var(--dsfr-outline)", width: 3 }),
+    ...(params?.placeholder
+      ? [
+          Placeholder.configure({
+            placeholder: params.placeholder,
+            emptyEditorClass: "tiptap-container--empty"
+          })
+        ]
+      : [])
+  ];
+}
