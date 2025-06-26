013dfbffd5b61b1a403249b7d52f58deb4001916
Adrien Boutigny <adrien@slash-tmp.dev>
2025-06-26 11:43:53 +0200

    add useFormField composable

---
+++ confiture-web-app/src/composables/validation.ts
@@ -0,0 +1,53 @@
+import { ComponentPublicInstance, ref } from "vue";
+
+export interface FocusableElement {
+  focus(): void;
+}
+
+type ValidationRule = (fieldValue: string) => string | false;
+
+export function useFormField(defaultValue: string, rules?: ValidationRule[]) {
+  const fieldInputRef = ref<FocusableElement | null>();
+  const fieldValue = ref<string>(defaultValue);
+  const fieldError = ref<string>();
+
+  function validate() {
+    fieldError.value = undefined;
+
+    if (!rules) return true;
+
+    for (let i = 0; i < rules.length && !fieldError.value; i++) {
+      fieldError.value = rules[i](fieldValue.value) || undefined;
+    }
+
+    if (fieldError.value) {
+      fieldInputRef.value?.focus();
+    }
+
+    return !fieldError.value;
+  }
+
+  return {
+    focusRef: fieldInputRef,
+    refFn: (el: Element | ComponentPublicInstance | null) => {
+      fieldInputRef.value = el as unknown as FocusableElement;
+    },
+    value: fieldValue,
+    error: fieldError,
+    validate
+  };
+}
+
+export function validate(...fields: ReturnType<typeof useFormField>[]) {
+  return !fields.map((it) => it.validate).some((it) => !it);
+}
+
+/* Validation rules */
+
+export function REQUIRED(msg: string): ValidationRule {
+  return (value) => !value.trim() && msg;
+}
+
+export function LENGTH(minLength: number, msg: string): ValidationRule {
+  return (value) => value.trim().length < minLength && msg;
+}
