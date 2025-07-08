e4b8f3c14df3de4ff8c1b5ac2221801479083474
Yaacov <yaacov@goodimpact.studio>
2025-07-08 14:09:48 +0200

    chore(ts): fix TS errors

--- confiture-web-app/src/store/notification.ts
+++ confiture-web-app/src/store/notification.ts
@@ -15,7 +15,7 @@ interface NotificationStoreState {
   } | null;
 }
 
-interface NotificationOptions {
+export interface NotificationOptions {
   action?: { label: string; cb: () => void };
   link?: { label: string; to: RouteLocationRaw };
 }
