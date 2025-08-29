98d2fe7f1408d884bcf885b2d75b017e796234a4
Yaacov <yaacov@goodimpact.studio>
2025-08-29 12:19:27 +0200

    fix(html): remove attribute when not necessary

--- confiture-web-app/src/components/audit/NotesModal.vue
+++ confiture-web-app/src/components/audit/NotesModal.vue
@@ -123,7 +123,7 @@ function handleDeleteFile(file: AuditFile) {
                 ref="fileUpload"
                 class="fr-mb-4w"
                 :audit-files="files"
-                :disabled="isOffline"
+                :disabled="isOffline ? true : null"
                 :error-message="errorMessage"
                 :multiple="true"
                 @upload-file="handleUploadFile"
