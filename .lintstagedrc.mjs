260cea8ede46a4dcd9036afb2d2904a260f6b1d0
Quentin Bellanger <hello@quentin-bellanger.com>
2025-09-04 14:54:16 +0200

    move stylelint to root project and plug lint-staged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
   // --no-warn-ignored option prevents lint-staged from displaying a warning
   // because "yarn.lock" is ignored
-  "*": () => ["eslint . --no-warn-ignored"]
+  "*": () => ["eslint . --no-warn-ignored", "yarn lint:styles"]
 };
