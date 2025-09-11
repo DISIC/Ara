db799c9ce8c545138e3edf75d80da411758130b1
Quentin Bellanger <hello@quentin-bellanger.com>
2025-09-11 12:08:04 +0200

    update email templates wording

--- confiture-rest-api/src/mail/account-verification-email.ts
+++ confiture-rest-api/src/mail/account-verification-email.ts
@@ -5,7 +5,7 @@ export interface AccountVerificationEmailData {
 }
 
 export function subject(): string {
-  return `Ara : vérification du compte`;
+  return `Confirmer votre adresse e-mail`;
 }
 
 export function html(data: AccountVerificationEmailData): string {
@@ -16,10 +16,13 @@ export function plain(data: AccountVerificationEmailData): string {
   return `
     Bonjour,
 
-    Pour finalisez la création de votre compte, veuillez cliquer sur le lien suivant : ${data.verificationLink}. Ce lien est valable 1h.
+    Pour finaliser la création de votre compte, cliquez sur le bouton ci-dessous :
+    
+    ${data.verificationLink}
+    Cette action est possible pendant 1 heure.
 
-    Si vous ne voulez pas créer de compte sur Ara ou si vous n’avez pas demandé à créer de compte, vous pouvez ignorer et supprimer cet e-mail.
+    Si vous n’avez pas demandé la création de ce compte, vous pouvez ignorer et supprimer cet e-mail.
 
-    Si vous avez des questions ou besoin d’aide, contactez notre support par e-mail à l’adresse : ara@design.numerique.gouv.fr.
+    Besoin d’aide ? Écrivez-nous : ara@design.numerique.gouv.fr
   `;
 }
