0e7dcae6cacb8f2578f95cd30b3b491a32f922b7
Adrien Boutigny <adrien@slash-tmp.dev>
2025-10-01 15:32:33 +0200

    Update .lintstagedrc.mjs

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,9 @@
+// --no-warn-ignored option prevents lint-staged from displaying a warning
+// because "yarn.lock" is ignored
+const eslint = "eslint --no-warn-ignored --fix"
+const stylelint = "stylelint --fix"
 export default {
-  // --no-warn-ignored option prevents lint-staged from displaying a warning
-  // because "yarn.lock" is ignored
-  "*.css": ["stylelint"],
-  "*.vue": ["stylelint", "eslint --no-warn-ignored"],
-  "!*.{css,vue}": ["eslint --no-warn-ignored"]
+  "*.css": [stylelint],
+  "*.vue": [stylelint, eslint],
+  "!*.{css,vue}": [eslint]
 };
