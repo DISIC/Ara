dcafd0f77cdf4496f71ce0e659b0ec0ad7098e4d
Adrien Boutigny <adrien@slash-tmp.dev>
2025-10-01 15:36:01 +0200

    add semicolons

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,7 @@
 // --no-warn-ignored option prevents lint-staged from displaying a warning
 // because "yarn.lock" is ignored
-const eslint = "eslint --no-warn-ignored --fix"
-const stylelint = "stylelint --fix"
+const eslint = "eslint --no-warn-ignored --fix";
+const stylelint = "stylelint --fix";
 export default {
   "*.css": [stylelint],
   "*.vue": [stylelint, eslint],
