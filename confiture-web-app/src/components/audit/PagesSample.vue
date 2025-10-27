DISIC/Ara

commit 28de3a0829611e606288e6b8b161f04726ac83b3

Simplifie l’écriture de formulaires validés (#1223)
* add DsfrFieldWithValidation and FormWithValitation

* validate always focus the first invalid element of the document

* move validation components to folder components/validation/

* update example

* pr feedback

* don’t sort the whole focusable elements array

* migrate AuditDeclarationPage (except environments section)

* migrate LoginPage

* pass attributes through in DsfrFieldWithValidation

* migrate FeedbackPage

* add #hint and #trailing slots to DsfrFieldWithValidation

* migrate AuditSettingsForm

* migrate NewAuditPages

* migrate NewAuditType

* add setError() exposed method on FielValidation

* migrate Password

* add exposed method setError in DsfrFieldWithValidation

* migrate Email

* migrate Account

* migrate RequestPasswordReset

* add DsfrPasswordWithValidation component

* migrate NewPasswordForm

* use DsfrPasswordWithValidation

* migrate NewAccountForm

* remove validation test page

* Update confiture-web-app/src/components/account/settings/Account.vue

Co-authored-by: Quentin Bellanger <hello@quentin-bellanger.com>

* update validation doc

---------

Co-authored-by: Quentin Bellanger <hello@quentin-bellanger.com>

confiture-web-app/src/components/audit/PagesSample.vue
@@ -6,6 +6,7 @@ import { REQUIRED, URL } from "../../composables/validation";
 import { AuditPage } from "../../types";
 import { URL_REGEX } from "../../utils";
 import DsfrField from "../ui/DsfrField.vue";
+import DsfrFieldWithValidation from "../validation/DsfrFieldWithValidation.vue";
 
 const props = defineProps<{
   modelValue: Omit<AuditPage, "id" | "order">[];
@@ -21,15 +22,13 @@ defineExpose({
     pageNameFieldRefs.value[
       pageNameFieldRefs.value.length - 1
     ].inputRef?.focus();
-  },
-  validate
+  }
 });
 
 const notify = useNotifications();
 
 const positionSuccessMessage = ref("");
 const pageNameFieldRefs = ref<InstanceType<typeof DsfrField>[]>([]);
-const pageUrlFieldRefs = ref<InstanceType<typeof DsfrField>[]>([]);
 
 const pages = ref(props.modelValue);
 const pageNameErrors = ref<Array<string | undefined>>(
@@ -115,32 +114,8 @@ function updatePageOrder(startIndex: number, endIndex: number) {
   } sur ${pages.value.length}`;
 }
 
-/** Validate page fields and focus the first invalid one if any. */
-function validate(): boolean {
-  const nameIsRequired = REQUIRED("Champ obligatoire. Saisissez le nom de la page à auditer.");
-  const urlIsRequired = REQUIRED("Champ obligatoire. Saisissez l’URL de la page à auditer.");
-  const urlIsValid = URL("Format incorrect. Saisissez une URL commençant par https:// ou http://");
-
-  let isValid = true;
-
-  for (let i = pages.value.length - 1; i >= 0; i--) {
-    const { name, url } = pages.value[i];
-
-    pageNameErrors.value[i] = nameIsRequired(name) || undefined;
-    pageUrlErrors.value[i] = urlIsRequired(url) || urlIsValid(url) || undefined;
-
-    if (pageUrlErrors.value[i]) {
-      pageUrlFieldRefs.value.at(i)?.focus();
-      isValid = false;
-    }
-
-    if (pageNameErrors.value[i]) {
-      pageNameFieldRefs.value.at(i)?.focus();
-      isValid = false;
-    }
-  }
-  return isValid;
-}
+const nameValidation = [REQUIRED("Champ obligatoire. Saisissez le nom de la page à auditer.")];
+const urlValidation = [REQUIRED("Champ obligatoire. Saisissez l’URL de la page à auditer."), URL("Format incorrect. Saisissez une URL commençant par https:// ou http://")];
 </script>
 
 <template>
@@ -193,30 +168,28 @@ function validate(): boolean {
         </div>
       </div>
 
-      <DsfrField
+      <DsfrFieldWithValidation
         :id="`page-name-${i + 1}`"
-        ref="pageNameFieldRefs"
         v-model="page.name"
         label="Nom de la page"
         class="fr-mt-7w"
-        :error="pageNameErrors[i]"
+        :validation="nameValidation"
       />
 
-      <DsfrField
+      <DsfrFieldWithValidation
         :id="`page-url-${i + 1}`"
-        ref="pageUrlFieldRefs"
         v-model="page.url"
         label="URL de la page"
         type="text"
         required
         :pattern="URL_REGEX"
-        :error="pageUrlErrors[i]"
+        :validation="urlValidation"
       >
         <template #hint>
           Saisissez une URL commençant par <code>https://</code> ou
           <code>http://</code>
         </template>
-      </DsfrField>
+      </dsfrfieldwithvalidation>
     </fieldset>
   </div>
 </template>

confiture-web-app/src/pages/account/LoginPage.vue
@@ -1,17 +1,16 @@
 <script lang="ts" setup>
 import { HTTPError } from "ky";
-import { nextTick, ref } from "vue";
+import { nextTick, ref, useTemplateRef } from "vue";
 import { useRouter } from "vue-router";
 
 import PageMeta from "../../components/PageMeta";
-import DsfrField from "../../components/ui/DsfrField.vue";
-import DsfrPassword from "../../components/ui/DsfrPassword.vue";
+import DsfrFieldWithValidation from "../../components/validation/DsfrFieldWithValidation.vue";
+import DsfrPasswordWithValidation from "../../components/validation/DsfrPasswordWithValidation.vue";
+import FormWithValidation from "../../components/validation/form-with-validation/FormWithValidation.vue";
 import { useNotifications } from "../../composables/useNotifications";
 import {
   EMAIL,
-  REQUIRED,
-  useFormField,
-  validate
+  REQUIRED
 } from "../../composables/validation";
 import { history } from "../../router";
 import { useAccountStore } from "../../store/account";
@@ -20,23 +19,19 @@ import { captureWithPayloads } from "../../utils";
 const showGenericLoginError = ref(false);
 const genericErrorRef = ref<HTMLDivElement>();
 
-const userEmail = useFormField<string>((history.state.email as string) ?? "", [
-  REQUIRED("Champ obligatoire. Saisissez votre adresse e-mail."),
-  EMAIL("Format incorrect. Utilisez le format : nom@domaine.fr.")
-]);
-
-const userPassword = useFormField<string>("", [
-  REQUIRED("Champ obligatoire. Saisissez votre mot de passe.")
-]);
+const userEmail = ref("");
+const userPassword = ref("");
 
 const showCreatedAccountAlert = ref(!!history.state.email);
 const showPasswordResetAlert = ref(history.state.passwordReset);
+
+const emailFieldRef = useTemplateRef("emailField");
 
 async function closeAlert() {
   showCreatedAccountAlert.value = false;
   showPasswordResetAlert.value = false;
   await nextTick();
-  userEmail.focusRef.value?.focus();
+  emailFieldRef.value?.focus();
 }
 
 const store = useAccountStore();
@@ -44,12 +39,8 @@ const router = useRouter();
 const notify = useNotifications();
 
 async function handleSubmit() {
-  if (!validate(userEmail, userPassword)) {
-    return;
-  }
-
   store
-    .login(userEmail.value.value, userPassword.value.value)
+    .login(userEmail.value, userPassword.value)
     .then(() => {
       router.push({ name: "account-dashboard" });
     })
@@ -97,43 +88,41 @@ async function handleSubmit() {
   </div>
 
   <div class="wrapper">
-    <form novalidate @submit.prevent="handleSubmit">
+    <FormWithValidation @submit="handleSubmit">
       <h1 class="fr-h3">Connexion à Ara</h1>
 
       <div v-if="showGenericLoginError" ref="genericErrorRef" tabindex="-1" class="fr-alert fr-alert--sm fr-alert--error fr-mb-3w">
         <p>L’adresse e-mail ou le mot de passe saisi est incorrect. Vérifiez vos saisies.</p>
       </div>
 
-      <DsfrField
+      <DsfrFieldWithValidation
         id="user-email"
-        :ref="userEmail.refFn"
-        :model-value="userEmail.value.value"
+        ref="emailField"
+        v-model="userEmail"
         label="Adresse e-mail"
         hint="Format attendu : nom@domaine.fr"
         type="email"
         required
         autocomplete="email"
-        :error="userEmail.error.value"
-        @update:model-value="userEmail.value.value = $event"
+        :validation="[REQUIRED('Champ obligatoire. Saisissez votre adresse e-mail.'),
+                      EMAIL('Format incorrect. Utilisez le format : nom@domaine.fr.')]"
       />
 
-      <DsfrPassword
+      <DsfrPasswordWithValidation
         id="user-password"
-        :ref="userPassword.refFn"
-        :model-value="userPassword.value.value"
+        v-model="userPassword"
         class="fr-my-3w"
-        :error="userPassword.error.value"
         label="Mot de passe"
         required
         autocomplete="current-password"
         show-forgotten-password-link
-        @update:model-value="userPassword.value.value = $event"
+        :validation="[REQUIRED('Champ obligatoire. Saisissez votre mot de passe.')]"
       />
 
       <div class="fr-btns-group">
         <button class="fr-btn fr-mb-0">Se connecter</button>
       </div>
-    </form>
+    </FormWithValidation>
 
     <div>
       <hr class="fr-mt-3w" />
@@ -148,8 +137,7 @@ async function handleSubmit() {
           Créer un compte
         </RouterLink>
       </div>
-    </div>
-  </div>
+    </div></div>
 </template>
 
 <style scoped>

confiture-web-app/src/components/account/new-acccount/NewAccountForm.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
 import { HTTPError } from "ky";
-import { ref } from "vue";
+import { ref, useTemplateRef } from "vue";
 
 import { useDevMode } from "../../../composables/useDevMode";
 import { useNotifications } from "../../../composables/useNotifications";
 import {
   EMAIL,
   LENGTH,
-  REQUIRED,
-  useFormField,
-  validate
+  REQUIRED
 } from "../../../composables/validation";
 import { useAccountStore } from "../../../store/account";
 import { captureWithPayloads, formatEmail } from "../../../utils";
-import DsfrField from "../../ui/DsfrField.vue";
-import DsfrPassword from "../../ui/DsfrPassword.vue";
+import DsfrFieldWithValidation from "../../validation/DsfrFieldWithValidation.vue";
+import DsfrPasswordWithValidation from "../../validation/DsfrPasswordWithValidation.vue";
+import FormWithValidation from "../../validation/form-with-validation/FormWithValidation.vue";
 
 const emit = defineEmits<{
   (e: "submit", payload: { username: string }): void;
@@ -22,32 +21,19 @@ const emit = defineEmits<{
 
 const togglePasswordRef = ref<HTMLInputElement>();
 
-const userEmail = useFormField<string>((history.state.email as string) ?? "", [
-  REQUIRED("Champ obligatoire. Saisissez votre adresse e-mail."),
-  EMAIL("Format incorrect. Utilisez le format : nom@domaine.fr.")
-]);
+const userEmail = ref((history.state.email as string) ?? "");
+const userPassword = ref("");
 
-const userPassword = useFormField<string>("", [
-  REQUIRED("Champ obligatoire. Saisissez un mot de passe."),
-  LENGTH(
-    12,
-    "Le nombre de caractères du mot de passe n’est pas suffisant. Veuillez choisir un mot de passe de 12 caractères minimum."
-  )
-]);
+const emailField = useTemplateRef("email-field");
 
 const accountStore = useAccountStore();
 const notify = useNotifications();
 
 async function handleSubmit() {
-  if (!validate(userEmail, userPassword)) {
-    // Invalid form
-    return;
-  }
-
   await accountStore
-    .createAccount(formatEmail(userEmail.value.value), userPassword.value.value)
+    .createAccount(formatEmail(userEmail.value), userPassword.value)
     .then(() => {
-      emit("submit", { username: userEmail.value.value });
+      emit("submit", { username: userEmail.value });
 
       if (togglePasswordRef.value) {
         togglePasswordRef.value.value = "false";
@@ -59,17 +45,12 @@ async function handleSubmit() {
 
         if (err.response.status === 409) {
           // Email already used
-          userEmail.error.value =
-            "Cette adresse e-mail est déjà associée à un compte. Connectez-vous.";
-          userEmail.focusRef.value?.focus();
+          emailField.value?.setError("Cette adresse e-mail est déjà associée à un compte. Connectez-vous.", true);
         } else if (
           err.response.status === 400 &&
           body.message.includes("username must be an email")
         ) {
-          // Invalid email format
-          userEmail.error.value =
-            "Format incorrect. Utilisez le format : nom@domaine.fr.";
-          userEmail.focusRef.value?.focus();
+          emailField.value?.setError("Format incorrect. Utilisez le format : nom@domaine.fr.", true);
         } else {
           // Unkown error
           notify(
@@ -87,8 +68,8 @@ const isDevMode = useDevMode();
 
 function fillFields() {
   const randomNumber = Math.floor(Math.random() * 1_000_000);
-  userEmail.value.value = `email-${randomNumber}@example.com`;
-  userPassword.value.value = "123blabla!!!Pouet";
+  userEmail.value = `email-${randomNumber}@example.com`;
+  userPassword.value = "123blabla!!!Pouet";
 }
 </script>
 
@@ -97,44 +78,47 @@ function fillFields() {
     [DEV] Remplir les champs
   </button>
   <div class="wrapper">
-    <form novalidate @submit.prevent="handleSubmit">
+    <FormWithValidation @submit="handleSubmit">
       <h1 tabindex="-1" class="fr-mb-3w fr-h3">Créer votre compte Ara</h1>
 
       <p class="fr-text--sm fr-mb-2w mandatory-notice">
         Sauf mention contraire, tous les champs sont obligatoires.
       </p>
 
-      <DsfrField
+      <DsfrFieldWithValidation
         id="user-email"
-        :ref="userEmail.refFn"
-        :model-value="userEmail.value.value"
+        ref="email-field"
+        v-model="userEmail"
         class="fr-mb-2w"
         label="Adresse e-mail"
         hint="Format attendu : nom@domaine.fr"
         type="email"
         required
-        :error="userEmail.error.value"
-        @update:model-value="userEmail.value.value = $event"
+        :validation="[
+          REQUIRED('Champ obligatoire. Saisissez votre adresse e-mail.'),
+          EMAIL('Format incorrect. Utilisez le format : nom@domaine.fr.')
+        ]"
       />
 
-      <DsfrPassword
+      <DsfrPasswordWithValidation
         id="user-password"
-        :ref="userPassword.refFn"
-        :model-value="userPassword.value.value"
+        v-model="userPassword"
         class="fr-mb-3w"
         label="Mot de passe"
-        :error="userPassword.error.value"
         autocomplete="new-password"
         :min-length="12"
         required
         :requirements="['12 caractères minimum']"
-        @update:model-value="userPassword.value.value = $event"
+        :validation="[
+          REQUIRED('Champ obligatoire. Saisissez un mot de passe.'),
+          LENGTH(12, 'Le nombre de caractères du mot de passe n’est pas suffisant. Veuillez choisir un mot de passe de 12 caractères minimum.')
+        ]"
       />
 
       <div class="fr-btns-group">
         <button class="fr-btn fr-mb-0" type="submit">Valider</button>
       </div>
-    </form>
+    </FormWithValidation>
   </div>
 </template>
 

confiture-web-app/src/components/account/password-reset/NewPasswordForm.vue
@@ -1,43 +1,36 @@
 <script lang="ts" setup>
-import { LENGTH, REQUIRED, useFormField, validate } from "../../../composables/validation";
-import DsfrPassword from "../../ui/DsfrPassword.vue";
+import { ref } from "vue";
+import { LENGTH, REQUIRED } from "../../../composables/validation";
+import DsfrPasswordWithValidation from "../../validation/DsfrPasswordWithValidation.vue";
+import FormWithValidation from "../../validation/form-with-validation/FormWithValidation.vue";
 
 const emit = defineEmits<{
   (e: "submit", newPassword: string): void;
 }>();
 
-const password = useFormField("" as string, [
-  REQUIRED("Champ obligatoire. Saisissez votre nouveau mot de passe. Il doit contenir 12 caractères minimum."),
-  LENGTH(12, "Le nombre de caractères du mot de passe n’est pas suffisant. Veuillez choisir un mot de passe de 12 caractères minimum.")
-]);
-
-function submitPassword() {
-  if (validate(password)) {
-    emit("submit", password.value.value);
-  }
-}
+const password = ref("");
 </script>
 
 <template>
-  <form
+  <FormWithValidation
     class="new-password-wrapper"
-    novalidate
-    @submit.prevent="submitPassword"
+    @submit="$emit('submit', password)"
   >
     <h1 class="fr-h3">Changer de mot de passe</h1>
 
-    <DsfrPassword
+    <DsfrPasswordWithValidation
       id="user-password-input"
-      :ref="password.refFn"
-      :model-value="password.value.value"
-      :error="password.error.value"
+      v-model="password"
       class="fr-mb-3w"
       label="Mot de passe"
       required
       autocomplete="new-password"
       :min-length="12"
       :requirements="['12 caractères minimum']"
-      @update:model-value="password.value.value = $event"
+      :validation="[
+        REQUIRED('Champ obligatoire. Saisissez votre nouveau mot de passe. Il doit contenir 12 caractères minimum.'),
+        LENGTH(12, 'Le nombre de caractères du mot de passe n’est pas suffisant. Veuillez choisir un mot de passe de 12 caractères minimum.')
+      ]"
     />
 
     <ul
@@ -52,7 +45,7 @@ function submitPassword() {
         <button type="submit" class="fr-btn">Changer de mot de passe</button>
       </li>
     </ul>
-  </form>
+  </FormWithValidation>
 </template>
 
 <style scoped>

confiture-web-app/src/components/account/password-reset/RequestPasswordReset.vue
@@ -1,47 +1,43 @@
 <script lang="ts" setup>
-import { EMAIL, REQUIRED, useFormField, validate } from "../../../composables/validation";
-import DsfrField from "../../ui/DsfrField.vue";
+import { ref, useTemplateRef } from "vue";
+import { EMAIL, REQUIRED } from "../../../composables/validation";
+import DsfrFieldWithValidation from "../../validation/DsfrFieldWithValidation.vue";
+import FormWithValidation from "../../validation/form-with-validation/FormWithValidation.vue";
 
 const emit = defineEmits<{
   (e: "submit", email: string): void;
 }>();
 
-const email = useFormField("" as string, [
-  REQUIRED("Champ obligatoire. Saisissez votre adresse e-mail."),
-  EMAIL("Format incorrect. Utilisez le format : nom@domaine.fr.")
-]);
+const email = ref("");
+const emailField = useTemplateRef("email-field");
 
 defineExpose({
   focusEmailField: () => {
-    email.focusRef.value?.focus();
+    emailField.value?.focus();
   }
 });
-
-function onSubmit() {
-  if (validate(email)) {
-    emit("submit", email.value.value);
-  }
-}
 </script>
 
 <template>
-  <form class="wrapper" novalidate @submit.prevent="onSubmit">
+  <FormWithValidation class="wrapper" @submit="$emit('submit', email)">
     <h1 class="fr-h3">Réinitialiser votre mot de passe</h1>
     <p class="fr-mb-2w">
       Saisissez l’adresse e-mail liée à votre compte. Vous recevrez un e-mail pour réinitialiser votre mot de passe.
     </p>
 
-    <DsfrField
+    <DsfrFieldWithValidation
       id="reset-password-email"
-      :ref="email.refFn"
+      ref="email-field"
+      v-model="email"
       label="Adresse e-mail"
       hint="Format attendu : nom@domaine.fr"
       type="email"
       required
-      :model-value="email.value.value"
-      :error="email.error.value"
       class="field-wrapper"
-      @update:model-value="email.value.value = $event"
+      :validation="[
+        REQUIRED('Champ obligatoire. Saisissez votre adresse e-mail.'),
+        EMAIL('Format incorrect. Utilisez le format : nom@domaine.fr.')
+      ]"
     />
 
     <ul
@@ -56,7 +52,7 @@ function onSubmit() {
         <button type="submit" class="fr-btn">Valider</button>
       </li>
     </ul>
-  </form>
+  </FormWithValidation>
 </template>
 
 <style scoped>

confiture-web-app/src/components/account/settings/Account.vue
@@ -1,42 +1,38 @@
 <script lang="ts" setup>
 import { HTTPError } from "ky";
-import { nextTick, ref } from "vue";
+import { nextTick, ref, useTemplateRef } from "vue";
 import { useRouter } from "vue-router";
 
 import { useNotifications } from "../../../composables/useNotifications";
-import { EQUAL, REQUIRED, useFormField, validate } from "../../../composables/validation";
+import { EQUAL, REQUIRED } from "../../../composables/validation";
 import { useAccountStore } from "../../../store/account";
 import { captureWithPayloads } from "../../../utils";
-import DsfrField from "../../ui/DsfrField.vue";
-import DsfrPassword from "../../ui/DsfrPassword.vue";
+import DsfrFieldWithValidation from "../../validation/DsfrFieldWithValidation.vue";
+import DsfrPasswordWithValidation from "../../validation/DsfrPasswordWithValidation.vue";
+import FormWithValidation from "../../validation/form-with-validation/FormWithValidation.vue";
 
 const router = useRouter();
 const accountStore = useAccountStore();
 const notify = useNotifications();
 
 const VALIDATION_STRING = "je confirme vouloir supprimer mon compte";
+const confirmPhraseValidation = [REQUIRED(`Champ obligatoire. Saisissez la phrase « ${VALIDATION_STRING} ».`), EQUAL(VALIDATION_STRING, "Saisie incorrecte. Vérifiez votre saisie.")];
+const confirmPhrase = ref("");
 
-const validation = useFormField("" as string, [REQUIRED(`Champ obligatoire. Saisissez la phrase « ${VALIDATION_STRING} ».`), EQUAL(VALIDATION_STRING, "Saisie incorecte. Vérifiez votre saisie.")]);
-const password = useFormField("" as string, [REQUIRED("Champ obligatoire. Saisissez votre mot de passe.")]);
+const passwordField = useTemplateRef("password-field");
+const password = ref("");
 
 const displayAccountDeletionForm = ref(false);
 
 async function deleteAccount() {
-  if (!validate(validation, password)) {
-    // Invalid form
-    return;
-  }
-
   accountStore
-    .deleteAccount(password.value.value)
+    .deleteAccount(password.value)
     .then(() => {
       router.push({ name: "account-deletion-feedback" });
     })
     .catch(async (e) => {
       if (e instanceof HTTPError && e.response.status === 401) {
-        password.error.value = "Saisie incorrecte. Vérifiez votre saisie.";
-        await nextTick();
-        password.focusRef.value?.focus();
+        passwordField.value?.setError("Saisie incorrecte. Vérifiez votre saisie.", true);
       } else {
         notify(
           "error",
@@ -73,29 +69,28 @@ async function hideAccountDeletionForm() {
         Toutes les données liées à votre compte seront définitivement supprimées.<br />Les audits et rapports que vous avez créés resteront accessibles, mais vos données personnelles ne seront plus mentionnées.
       </p>
     </div>
-    <form novalidate @submit.prevent="deleteAccount">
-      <DsfrField
-        id="confirm-sentence" :ref="validation.refFn"
-        :model-value="validation.value.value"
-        :error="validation.error.value"
+    <FormWithValidation @submit="deleteAccount">
+      <DsfrFieldWithValidation
+        id="confirm-sentence"
+        v-model="confirmPhrase"
         type="text"
         required
         :label="`Saisissez la phrase suivante pour confirmer la suppression de votre compte : ${VALIDATION_STRING}`"
         hint="N’utilisez pas de majuscules, ni d’espace au début ou à la fin de votre saisie."
-        @update:model-value="validation.value.value = $event"
+        :validation="confirmPhraseValidation"
       />
 
-      <DsfrPassword
+      <DsfrPasswordWithValidation
         id="account-password"
-        :ref="password.refFn"
-        :model-value="password.value.value"
-        :error="password.error.value"
+        ref="password-field"
+
+        v-model="password"
+        :validation="[REQUIRED('Champ obligatoire. Saisissez votre mot de passe.')]"
         label="Mot de passe"
         required
         autocomplete="current-password"
         show-forgotten-password-link
         skip-forgotten-password-first-step
-        @update:model-value="password.value.value = $event"
       />
 
       <ul
@@ -116,7 +111,7 @@ async function hideAccountDeletionForm() {
           </button>
         </li>
       </ul>
-    </form>
+    </FormWithValidation>
   </template>
   <button
     v-else

commit 0f2ef64f8d050e4b351a5d27c39310f4e3daba90

fix: issues introduced with PR #1223
Failures in e2e tests:
- `cy.contains("Ajouter une page").click()` (PagesSample#focusLastField function was broken)
- "User can reset their password" (and other tests): `cy.contains("button", "Se connecter").click()` → username was not prefilled

confiture-web-app/src/components/audit/PagesSample.vue
@@ -170,6 +170,7 @@ const urlValidation = [REQUIRED("Champ obligatoire. Saisissez l’URL de la page
 
       <DsfrFieldWithValidation
         :id="`page-name-${i + 1}`"
+        ref="pageNameFieldRefs"
         v-model="page.name"
         label="Nom de la page"
         class="fr-mt-7w"

confiture-web-app/src/pages/account/LoginPage.vue
@@ -19,7 +19,7 @@ import { captureWithPayloads } from "../../utils";
 const showGenericLoginError = ref(false);
 const genericErrorRef = ref<HTMLDivElement>();
 
-const userEmail = ref("");
+const userEmail = ref(history.state.email as string ?? "");
 const userPassword = ref("");
 
 const showCreatedAccountAlert = ref(!!history.state.email);