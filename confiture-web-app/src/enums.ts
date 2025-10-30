72e498c4226152750f236ccd0d243f1f00f8e97c
Quentin Bellanger <hello@quentin-bellanger.com>
2025-10-30 12:01:54 +0100

    add error when uploading more than 5 files

--- confiture-web-app/src/enums.ts
+++ confiture-web-app/src/enums.ts
@@ -31,7 +31,8 @@ export enum FileErrorMessage {
   UPLOAD_SIZE = "Votre fichier dépasse la limite de 2 Mo. Veuillez choisir un fichier plus léger.",
   UPLOAD_FORMAT = "Format de fichier non supporté.",
   UPLOAD_TIMEOUT = "Une erreur réseau empêche le téléchargement du fichier (expiration du délai d'attente). Veuillez réessayer.",
-  FETCH_ERROR = "Impossible de récupérer le fichier distant",
+  UPLAOD_MAX_FILES_COUNT = "Impossible de télécharger plus de 5 fichiers à la fois. Veuillez réessayer.",
+  FETCH_ERROR = "Impossible de récupérer le fichier distant.",
   UPLOAD_FORMAT_EXAMPLE = "Format de fichier non supporté. Veuillez choisir un fichier jpg, jpeg ou png.",
   UPLOAD_UNKNOWN = "Une erreur inconnue empêche le téléchargement du fichier. Veuillez réessayer.",
   DELETE_UNKNOWN = "Une erreur inconnue empêche la suppression du fichier. Veuillez réessayer."
