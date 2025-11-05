baf47aa968e768b9281f31083d4797954afcb057
Quentin Bellanger <hello@quentin-bellanger.com>
2025-11-05 15:38:33 +0100

    add prop for toplink top margin

--- confiture-web-app/src/components/audit/AuditGenerationPageCriteria.vue
+++ confiture-web-app/src/components/audit/AuditGenerationPageCriteria.vue
@@ -155,7 +155,7 @@ onMounted(() => {
         </ol>
 
         <div class="fr-grid-row fr-grid-row--right">
-          <TopLink target="audit-tabs" />
+          <TopLink target="audit-tabs" top-margin="1.5rem" />
         </div>
       </template>
     </section>
