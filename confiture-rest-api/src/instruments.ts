066a18124663ed798fe0eff7d121a48d4e8c05dc
Adrien Boutigny <adrien@slash-tmp.dev>
2025-11-06 14:40:08 +0100

    fix(api): remove console log in instruments.ts

--- confiture-rest-api/src/instruments.ts
+++ confiture-rest-api/src/instruments.ts
@@ -1,6 +1,5 @@
 import * as Sentry from "@sentry/nestjs";
 
-console.log("🚀 ~ process.env.SENTRY_DSN:", process.env.SENTRY_DSN);
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   environment: process.env.SENTRY_ENVIRONMENT ?? "unknown"
