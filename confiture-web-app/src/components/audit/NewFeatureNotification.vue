ab6893b4af1bc029c2ce22acb4f344eedaa044d6
Yaacov Cohen <yaacov@goodimpact.studio>
2025-11-14 11:20:34 +0100

    fix(NewFeatureNotification): size is md not sm

--- confiture-web-app/src/components/audit/NewFeatureNotification.vue
+++ confiture-web-app/src/components/audit/NewFeatureNotification.vue
@@ -23,7 +23,7 @@ function closeNotification() {
 </script>
 
 <template>
-  <div v-if="showNewFeatureNotification" class="fr-alert fr-alert--info fr-alert--sm fr-pb-2w notification">
+  <div v-if="showNewFeatureNotification" class="fr-alert fr-alert--info fr-pb-2w notification">
     <h3 class="fr-alert__title">Nouveauté : ajoutez vos images dans les zones de texte</h3>
     <p>L’ajout d’image se fait désormais directement dans la zone de texte, par copier-coller, glisser-déposer ou à l’aide du bouton « Insérer une image ».</p>
     <button class="fr-btn fr-btn--tertiary-no-outline fr-btn--sm notification-action" @click="closeNotification">Ne plus afficher</button>
