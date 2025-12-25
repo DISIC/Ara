27c772da568b5bcfb678d6d57cfbdc3d67562061
Adrien Boutigny <adrien@slash-tmp.dev>
2025-12-25 20:22:21 +0100

    fix prisma cli not working when DATABASE_URL is not set, even when it is not needed

--- confiture-rest-api/prisma.config.ts
+++ confiture-rest-api/prisma.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 import "dotenv/config";
+
+if (!process.env.DATABASE_URL) {
+  console.warn("No DATABASE_URL specified. Using placeholder value");
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,6 +11,8 @@ export default defineConfig({
     path: "prisma/migrations"
   },
   datasource: {
-    url: env("DATABASE_URL")
+    // sometimes a DATABASE_URL in not specified
+    // we use a placeholder so prisma does not complain when generation the client
+    url: process.env.DATABASE_URL || "placeholder_database_url"
   }
 });
