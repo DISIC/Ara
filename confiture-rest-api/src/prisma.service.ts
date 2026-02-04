8a319b0c4e5a47e2a44606fe88f9599057c0bbc7
Adrien Boutigny <adrien@slash-tmp.dev>
2026-02-04 16:38:51 +0100

    fix unhandled error when generating api types

--- confiture-rest-api/src/prisma.service.ts
+++ confiture-rest-api/src/prisma.service.ts
@@ -8,21 +8,27 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor(
     private readonly config: ConfigService
   ) {
-    const originalUrl = config.get<string>("DATABASE_URL");
-    const url = new URL(originalUrl);
+    let url = config.get<string>("DATABASE_URL");
 
     // add or remove url params based on weither we want to connet to the DB using SSL
-    if (config.get<boolean>("DATABASE_SSL")) {
-      url.searchParams.set("sslmode", "require");
-      url.searchParams.set("uselibpqcompat", "true");
-    } else {
-      url.searchParams.delete("sslmode");
-      url.searchParams.delete("uselibpqcompat");
+    try {
+      const urlBuilder = new URL(url);
+      if (config.get<boolean>("DATABASE_SSL")) {
+        urlBuilder.searchParams.set("sslmode", "require");
+        urlBuilder.searchParams.set("uselibpqcompat", "true");
+      } else {
+        urlBuilder.searchParams.delete("sslmode");
+        urlBuilder.searchParams.delete("uselibpqcompat");
+      }
+      url = urlBuilder.toString();
+    } catch {
+      // if we fail to parse the url, do nothing, there are cases where
+      // DATABASE_URL is undefined (such as when generating types)
     }
 
     super({
       adapter: new PrismaPg({
-        connectionString: url.toString()
+        connectionString: url
       })
     });
   }
