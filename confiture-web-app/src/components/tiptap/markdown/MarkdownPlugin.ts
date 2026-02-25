fa116d1c9d287410a9b1d946c84b3606e805f645
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-02-25 16:50:08 +0100

    Gestion des headings avec markdown

--- confiture-web-app/src/components/tiptap/markdown/MarkdownPlugin.ts
+++ confiture-web-app/src/components/tiptap/markdown/MarkdownPlugin.ts
@@ -1,5 +1,6 @@
 import { Editor } from "@tiptap/core";
 import { Plugin, PluginKey } from "@tiptap/pm/state";
+import { uniqBy } from "lodash-es";
 
 // Docs :  https://tiptap.dev/docs/editor/markdown/examples
 
@@ -32,14 +33,9 @@ export class MarkdownPlugin extends Plugin {
         return false;
       }
 
-      const json = this.editor.markdown.parse(text);
+      const markdown = this.realignCommentHeadings(text);
 
-      // Mapping headings h1, h2, h3 to h4, h5, h6
-      if (json.content?.some(x => x.type === "heading")) {
-        json.content
-          .filter((x) => x.type === "heading")
-          .forEach((item) => this.remapHeadingLevels(item));
-      }
+      const json = this.editor.markdown.parse(markdown);
 
       editor.commands.insertContent(json);
       return true;
@@ -62,19 +58,66 @@ export class MarkdownPlugin extends Plugin {
     );
   }
 
-  private remapHeadingLevels(node: any): void {
-    if (node.attrs) {
-      const mappedLevel = this.mapHeadingLevel(node.attrs.level);
-      if (mappedLevel) {
-        node.attrs = { ...node.attrs, level: mappedLevel };
+  private realignCommentHeadings(markdown: string): string {
+    if (!markdown) {
+      return markdown;
+    }
+
+    const lines: [number, string][] = markdown.split("\n").map((l, i) => [i, l]);
+
+    // Ignore code blocks for following heading level calculation
+    // (in case of markdown in code block)
+    const linesWithoutCodeBlocks: [number, string][] = [];
+    let ignore = false;
+    for (let i = 0; i < lines.length; i++) {
+      const it = lines[i];
+      if (it[1].startsWith("```") && !it[1].slice(3).endsWith("```")) {
+        ignore = !ignore;
+      }
+      if (!ignore) {
+        linesWithoutCodeBlocks.push([it[0], it[1].trim()]);
       }
     }
-  }
 
-  private mapHeadingLevel(level?: number): number | null {
-    if (level === 1) return 4;
-    if (level === 2) return 5;
-    if (level === 3) return 6;
-    return level ?? null;
+    const headingLines = linesWithoutCodeBlocks.filter(([, l]) =>
+      /^#+ ./.test(l)
+    );
+
+    const linesWithLevels = headingLines.map(([i, l]) => ({
+      index: i,
+      line: l,
+      level: l.split(" ").at(0)?.length ?? 0
+    }));
+
+    const headingLevels = uniqBy(linesWithLevels, (it) => it.level).map(
+      (it) => it.level
+    );
+    headingLevels.sort();
+
+    for (let i = 0; i < headingLevels.length; i++) {
+      const originalLevel = headingLevels[i];
+      const newLevel = 4 + i;
+      if (newLevel <= 6) {
+        linesWithLevels
+          .filter((it) => it.level === originalLevel)
+          .forEach((it) => {
+            it.line = it.line.replace(/^#+/, "#".repeat(newLevel));
+          });
+      } else {
+        linesWithLevels
+          .filter((it) => it.level === originalLevel)
+          .forEach((it) => {
+            it.line = it.line.replace(/^#+ /, "".repeat(newLevel));
+          });
+      }
+    }
+
+    linesWithLevels.forEach((it) => {
+      lines[it.index][1] = it.line;
+    });
+
+    const result = lines.map(([, line]) => line).join("\n");
+
+    return result;
   }
 }
