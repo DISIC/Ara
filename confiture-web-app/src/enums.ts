23c32c30a767ac79551af41310e808eb6516cf13
Quentin Bellanger <hello@quentin-bellanger.com>
2026-02-25 15:05:34 +0100

    wording: update old contact email address

--- confiture-web-app/src/enums.ts
+++ confiture-web-app/src/enums.ts
@@ -41,7 +41,7 @@ export enum FileErrorMessage {
 };
 
 export const DEFAULT_NOTIFICATION_ERROR_TITLE = "Erreur inconnue";
-export const DEFAULT_NOTIFICATION_ERROR_DESCRIPTION = "Réessayez. Si le problème persiste, contactez-nous : ara@design.numérique.fr";
+export const DEFAULT_NOTIFICATION_ERROR_DESCRIPTION = "Réessayez. Si le problème persiste, contactez-nous : ara@design.numerique.gouv.fr";
 
 export enum StaticTabLabel {
   AUDIT_COMMON_ELEMENTS_TAB_LABEL = "Éléments transverses",
