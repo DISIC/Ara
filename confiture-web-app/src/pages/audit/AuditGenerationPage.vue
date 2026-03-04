415ad6f369f442662c015040651b2f96ddd731c2
Quentin Bellanger <hello@quentin-bellanger.com>
2026-03-04 14:43:52 +0100

    wording: update comleted page hidden tab suffix(2)

--- confiture-web-app/src/pages/audit/AuditGenerationPage.vue
+++ confiture-web-app/src/pages/audit/AuditGenerationPage.vue
@@ -211,7 +211,7 @@ const tabsData = computed((): TabData[] => {
       : []),
     ...(auditStore.currentAudit?.pages.map((p) => ({
       label: p.name,
-      hiddenLabelSuffix: resultsStore.isPageCompleted(p.id) ? " entièrement évalué" : undefined,
+      hiddenLabelSuffix: resultsStore.isPageCompleted(p.id) ? " (entièrement évalué)" : undefined,
       id: p.id,
       icon: resultsStore.isPageCompleted(p.id) ? "fr-icon-check-line" : undefined,
       component: AuditGenerationPageCriteria,
