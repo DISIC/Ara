36e08f4f5f16ed37aaa442a42afc226bf57573be
Quentin Bellanger <hello@quentin-bellanger.com>
2026-03-18 18:09:15 +0100

    fix: adjust audit row spacing...

--- confiture-web-app/src/components/account/dashboard/AuditsList.vue
+++ confiture-web-app/src/components/account/dashboard/AuditsList.vue
@@ -210,7 +210,7 @@ async function setFocusAfterAction() {
 
 .headers {
   display: grid;
-  grid-template-columns: 1.75fr 0.5fr 0.75fr 1.25fr 0.75fr 1.5fr 0.75fr 0.25fr;
+  grid-template-columns: 1.75fr 0.5fr 0.75fr 1.1fr 0.6fr 1.5fr 0.75fr 0.25fr;
   gap: 1rem;
 
   @media (width < 55rem) {
