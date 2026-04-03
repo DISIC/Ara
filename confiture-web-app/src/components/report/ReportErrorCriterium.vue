14d5a67d4459ab928085efaaf12581f9087d8cd1
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-04-03 17:54:14 +0200

    afficher les erreurs sur la page rapport

--- confiture-web-app/src/components/report/ReportErrorCriterium.vue
+++ confiture-web-app/src/components/report/ReportErrorCriterium.vue
@@ -33,11 +33,17 @@ function getCriteriumTitle(topicNumber: number, criteriumNumber: number) {
 }
 
 const sectionId = computed(() => `${error.pageId}_${error.topic}_${error.criterium}`);
+
+const countNotCompliantItemsRecommandations = computed(() => {
+  return error.notCompliantItems
+    .filter(x => x.comment || x.quickWin === true || x.title || x.userImpact)
+    .length;
+});
 </script>
 
 <template>
   <div>
-    <p :id="sectionId" class="fr-text--lg fr-text--bold criterium-title fr-mb-2w">
+    <p :id="sectionId" class="fr-text--lg fr-text--bold criterium-title fr-h3 fr-mb-2w">
       {{ error.topic }}.{{ error.criterium }}&nbsp;–
       <span v-html="getCriteriumTitle(error.topic, error.criterium)" />
       <a :href="'#' + sectionId" class="fr-btn fr-icon-links-line fr-btn--tertiary-no-outline fr-btn--sm">
@@ -45,43 +51,64 @@ const sectionId = computed(() => `${error.pageId}_${error.topic}_${error.criteri
       </a>
     </p>
 
-    <ul class="fr-badges-group fr-mb-3w">
-      <li>
-        <p class="fr-badge fr-badge--sm fr-badge--error fr-badge--no-icon">
-          {{ formatStatus(error.status) }}
-        </p>
-      </li>
-      <li v-if="error.userImpact">
-        <p
-          class="fr-badge fr-badge--sm"
-          :class="{
-            'fr-badge--yellow-moutarde':
-              error.userImpact === CriterionResultUserImpact.MAJOR,
-            'fr-badge--error fr-badge--no-icon':
-              error.userImpact === CriterionResultUserImpact.BLOCKING
-          }"
-        >
-          Impact {{ formatUserImpact(error.userImpact) }}
-        </p>
-      </li>
-      <li v-if="error.quickWin">
-        <p class="fr-badge fr-badge--sm">Facile à corriger</p>
-      </li>
-    </ul>
-
-    <!-- Error -->
-    <TiptapRenderer
-      v-if="
-        error.notCompliantComment &&
-          !isTiptapDocumentEmpty(error.notCompliantComment)
-      "
-      :key="error.topic + '.' + error.criterium"
-      :document="error.notCompliantComment"
-    />
-
-    <p v-else>
-      Aucune description de l’erreur ou recommandation de correction.
+    <p class="fr-badge fr-badge--sm fr-badge--error fr-badge--no-icon fr-mb-3w">
+      {{ formatStatus(error.status) }} ({{ countNotCompliantItemsRecommandations }})
     </p>
+
+    <div role="list">
+      <div
+        v-for="(notCompliantItem, index) in error.notCompliantItems
+          .filter((x) =>
+            x.comment || x.quickWin === true || x.title || x.userImpact)"
+        :key="index"
+        role="listitem"
+        class="criterium-not-compliant-item"
+      >
+        <div class="criterium-not-compliant-item-header">
+          <ul class="fr-badges-group fr-mb-3w">
+            <li v-if="notCompliantItem.userImpact">
+              <p
+                class="fr-badge fr-badge--sm fr-m-1-5v"
+                :class="{
+                  'fr-badge--yellow-moutarde':
+                    notCompliantItem.userImpact
+                    === CriterionResultUserImpact.MAJOR,
+                  'fr-badge--error fr-badge--no-icon':
+                    notCompliantItem.userImpact
+                    === CriterionResultUserImpact.BLOCKING
+                }"
+              >
+                Impact {{ formatUserImpact(notCompliantItem.userImpact) }}
+              </p>
+            </li>
+            <li v-if="notCompliantItem.quickWin">
+              <p class="fr-badge fr-badge--sm fr-m-1-5v">Facile à corriger</p>
+            </li>
+          </ul>
+
+          <a :href="`#${sectionId}_erreur_${index + 1}`" class="fr-btn fr-icon-links-line fr-btn--tertiary-no-outline fr-btn--sm">
+            <span class="fr-sr-only">ancre vers l'erreur {{ index + 1 }} du critère {{ error.topic }}.{{ error.criterium }}</span>
+          </a>
+        </div>
+
+        <h3 v-if="notCompliantItem.title" class="fr-h4">{{ notCompliantItem.title }}</h3>
+
+        <!-- Error -->
+        <TiptapRenderer
+          v-if="
+            notCompliantItem.comment &&
+              !isTiptapDocumentEmpty(notCompliantItem.comment)
+          "
+          :key="error.topic + '.' + error.criterium"
+          :document="notCompliantItem.comment"
+        />
+        <p v-else class="criterium-not-compliant-item-no-erreur">
+          Aucune description de l’erreur ou recommandation ajoutée par l’auditrice ou l’auditeur.
+        </p>
+
+      </div>
+
+    </div>
 
     <template v-if="chunk(error.exampleImages, 2).length">
       <p class="fr-text--xs fr-mb-1w error-accordion-subtitle">
@@ -124,5 +151,36 @@ const sectionId = computed(() => `${error.pageId}_${error.topic}_${error.criteri
 
 .criterium-title {
   scroll-margin: 4rem;
+}
+
+.criterium-not-compliant-item {
+  border: 1px solid var(--border-default-grey);
+  padding: 1rem;
+  margin-bottom: 2rem;
+}
+
+.criterium-not-compliant-item-header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.criterium-not-compliant-item-no-erreur {
+  font-style: italic;
+  color: var(--text-mention-grey);
+  text-align: center;
+}
+</style>
+
+<style>
+.criterium-not-compliant-item h4 {
+  font-size: 1.375rem;
+}
+
+.criterium-not-compliant-item h5 {
+  font-size: 1.25rem;
+}
+
+.criterium-not-compliant-item h6 {
+  font-size: 1.125rem;
 }
 </style>
