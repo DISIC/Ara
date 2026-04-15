2cc4d07f31839c3f93ddfc11a041fe94209d5425
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-04-15 15:36:26 +0200

    fix regression + accessibilité

--- confiture-rest-api/src/audits/audit.service.ts
+++ confiture-rest-api/src/audits/audit.service.ts
@@ -617,8 +617,14 @@ export class AuditService {
       .map((item) => {
         const result: any[] = [];
 
+        const newNotCompliantItems =
+          item.notCompliantItems?.filter((x) => !x.id).map((e) =>
+            omit(e, ["id", "criterionResultId"])
+          ) ?? [];
+
         const existingNotCompliantItems = item.notCompliantItems
           .filter((x) => x.id);
+
         if (existingNotCompliantItems.length) {
           const notCompliantItemsToUpdate = existingNotCompliantItems
             .map((notCompliantItem) => {
@@ -650,11 +656,6 @@ export class AuditService {
 
           result.push(notCompliantItemsToDelete);
         }
-
-        const newNotCompliantItems =
-          item.notCompliantItems?.filter((x) => !x.id).map((e) =>
-            omit(e, ["id", "criterionResultId"])
-          ) ?? [];
 
         const data: Prisma.CriterionResultUpsertArgs["create"] = {
           criterium: item.criterium,
