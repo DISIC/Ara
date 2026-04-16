ddc3c13a84c9fb71b9eb57124100fb8d7c572881
Quentin Bellanger <hello@quentin-bellanger.com>
2026-04-16 16:06:31 +0200

    chore: remove old comment

--- confiture-web-app/src/store/debug.ts
+++ confiture-web-app/src/store/debug.ts
@@ -2,10 +2,6 @@ import ky from "ky";
 import { defineStore } from "pinia";
 import { Audit, CreateDebugAuditRequestData } from "../types";
 
-// True on dev and review environments with "?dev=1"
-// TODO: only check we're in ?dev=1
-// because api route are disabled in production anyway
-
 export const useDebugStore = defineStore("debug", {
   actions: {
     async createDebugAudit(data: CreateDebugAuditRequestData) {
