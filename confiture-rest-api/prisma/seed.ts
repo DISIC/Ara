cc715681a116bc4529edef243fa6f30a99a3f9e1
Quentin Bellanger <hello@quentin-bellanger.com>
2026-04-22 13:41:09 +0200

    Améliorer les fonctionnalités de débug (#1473)
    * feat: add quick way to create a default test audit
    
    * feat: seed audits for seeded accounts
    
    * chore: exclude seed data file from tsconfig build
    
    * fix: increase prisma transaction timeout
    
    * fix: increase prisma transaction timeout again
    
    * fix: correctly create or update audits when seeding

--- confiture-rest-api/prisma/seed.ts
+++ confiture-rest-api/prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { hash } from "bcrypt";
 import { PrismaClient } from "../src/generated/prisma/client";
+import { getRawAccounts, rawAudits, rawCriteria } from "./seed-data";
 import "dotenv/config";
 
 const urlBuilder = new URL(process.env.DATABASE_URL);
@@ -19,43 +19,111 @@ const url = urlBuilder.toString();
 const adapter = new PrismaPg({ connectionString: url });
 const prisma = new PrismaClient({ adapter });
 
-async function generateAccounts() {
+async function generateSeeds() {
   if (!process.env.DATABASE_SEEDS) {
     console.log("Nothing to seeds");
     return;
   };
 
-  const accounts = await Promise.all(process.env.DATABASE_SEEDS.split(",").map(async acc => {
-    const [email, rawPassword] = acc.split(":");
+  const rawAccounts = await getRawAccounts();
 
-    if (!email || !rawPassword) {
-      throw new Error("Wrong seeds configuration (expected format: email:password");
-    }
+  await prisma.$transaction(async (tx) => {
+    const accounts = await Promise.all(rawAccounts.map(async rawAcc => {
+      const data = {
+        username: rawAcc.email,
+        password: rawAcc.password,
+        isVerified: true,
+        verificationJti: null
+      };
 
-    return {
-      email,
-      password: await hash(rawPassword, 10)
-    };
-  }));
+      return await tx.user.upsert({
+        where: {
+          username: rawAcc.email
+        },
+        create: data,
+        update: data
+      });
+    }));
 
-  await prisma.$transaction(accounts.map((acc) => {
-    const data = {
-      username: acc.email,
-      password: acc.password,
-      isVerified: true,
-      verificationJti: null
-    };
+    const audits = await Promise.all(accounts.flatMap((acc, i) => {
+      return rawAudits.map(rawAudit => {
+        // Example key: "0-full-pending-with-statement"
+        const uniqueKey = `${i}-${rawAudit.auditType.toLowerCase()}-${rawAudit.publicationDate ? "pending" : "completed"}${rawAudit.statementPublicationDate ? "-with-statement" : ""}`;
+        const editUniqueId = `edit-${uniqueKey}`;
+        const reportUniqueId = `report-${uniqueKey}`;
 
-    return prisma.user.upsert({
-      where: {
-        username: acc.email
-      },
-      create: data,
-      update: data
-    });
-  }));
+        const data = {
+          ...rawAudit,
+          editUniqueId,
+          consultUniqueId: reportUniqueId,
+          auditorEmail: acc.username,
+          auditTrace: {
+            connectOrCreate: {
+              where: {
+                auditEditUniqueId: editUniqueId
+              },
+              create: {
+                auditEditUniqueId: editUniqueId,
+                auditConsultUniqueId: reportUniqueId
+              }
+            }
+          },
+          pages: rawAudit.pages
+            ? {
+                createMany: {
+                  data: rawAudit.pages.createMany.data,
+                  skipDuplicates: true
+                }
+              }
+            : undefined,
+          environments: rawAudit.environments
+            ? {
+                createMany: {
+                  data: rawAudit.environments.createMany.data,
+                  skipDuplicates: true
+                }
+              }
+            : undefined
+        };
 
-  console.log(`Seeded ${accounts.length} accounts`);
+        const { pages, transverseElementsPage, environments, auditTrace, ...updateData } = data;
+
+        return tx.audit.upsert({
+          where: {
+            editUniqueId
+          },
+          create: { isHidden: false, ...data },
+          update: { isHidden: false, ...updateData },
+          select: {
+            publicationDate: true,
+            transverseElementsPage: true,
+            pages: true
+          }
+        });
+      });
+    }));
+
+    // Fill results for completed audits
+    const completedAudits = audits.filter(a => a.publicationDate);
+
+    await Promise.all(completedAudits.flatMap(audit => {
+      return [audit.transverseElementsPage, ...audit.pages].map(p => {
+        return tx.criterionResult.createMany({
+          data: rawCriteria.map(c => {
+            return {
+              ...c,
+              pageId: p.id
+            };
+          }),
+          skipDuplicates: true
+        });
+      });
+    }));
+  }, {
+    timeout: 20_000
+  });
+
+  console.log(`Seeded ${rawAccounts.length} accounts with ${rawAudits.length} audits each.`);
 }
 
-generateAccounts();
+generateSeeds();
