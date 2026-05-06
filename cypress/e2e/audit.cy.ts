f05411501e24e4075bfc59faf402129630a2bdf0
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-05-06 16:50:56 +0200

    fix tests

--- cypress/e2e/audit.cy.ts
+++ cypress/e2e/audit.cy.ts
@@ -632,7 +632,7 @@ describe("Audit", () => {
       cy.get(".criterium-container .not-compliant-item input[type='text']")
         .type("Absence de l'alt sur l'image");
 
-      cy.get(".criterium-container .not-compliant-item .tiptap.tiptap")
+      cy.get(".criterium-container .not-compliant-item .tiptap")
         .clear({ force: true })
         .type("Il n’y a pas de alt sur l’image du hero");
 
@@ -642,10 +642,8 @@ describe("Audit", () => {
   });
 
   it("User can add a new not compliant item", () => {
-    cy.createTestAudit().then(({ editId }) => {
+    cy.createTestAudit({ isPristine: true }).then(({ editId }) => {
       cy.visit(`http://localhost:3000/audits/${editId}/generation`);
-
-      cy.contains("button[role=\"tab\"]", "FAQ").click();
 
       cy.get(".criterium-container").contains("Non conforme");
 
@@ -656,7 +654,7 @@ describe("Audit", () => {
       cy.get(".criterium-container .not-compliant-item:last input[type='text']")
         .type("Absence de l'alt sur l'image");
 
-      cy.get(".criterium-container .not-compliant-item:last .tiptap.tiptap")
+      cy.get(".criterium-container .not-compliant-item:last .tiptap")
         .type("Il n’y a pas de alt sur l’image du hero");
 
       cy.get(".criterium-container .not-compliant-item:last label").contains("mineur").click();
@@ -668,10 +666,8 @@ describe("Audit", () => {
   });
 
   it("User can delete an not compliant item", () => {
-    cy.createTestAudit().then(({ editId }) => {
+    cy.createTestAudit({ isPristine: true }).then(({ editId }) => {
       cy.visit(`http://localhost:3000/audits/${editId}/generation`);
-
-      cy.contains("button[role=\"tab\"]", "FAQ").click();
 
       cy.get(".criterium-container").contains("Non conforme");
 
@@ -684,7 +680,7 @@ describe("Audit", () => {
       cy.get(".criterium-container .not-compliant-item:last input[type='text']")
         .type("Absence de l'alt sur l'image");
 
-      cy.get(".criterium-container .not-compliant-item:last .tiptap.tiptap")
+      cy.get(".criterium-container .not-compliant-item:last .tiptap")
         .type("Il n’y a pas de alt sur l’image du hero");
 
       cy.get(".criterium-container .not-compliant-item:last label").contains("mineur").click();
@@ -834,8 +830,8 @@ describe("Audit", () => {
 
     cy.createTestAudit({ isPristine: true }).then(({ editId, reportId }) => {
       cy.visit(`http://localhost:3000/audits/${editId}/generation`);
-      cy.get(".criterium-container").contains("Non conforme").click();
-      cy.wait("@updateResults");
+      cy.get(".criterium-container").contains("Non conforme");
+      cy.get(".criterium-container").contains("Erreurs et recommandations (1)").click();
 
       // 1. Insert an image into the editor with the button
       cy.log("** Insert 1 image with the button **");
@@ -909,8 +905,9 @@ describe("Audit", () => {
 
     cy.createTestAudit({ isPristine: true }).then(({ editId, reportId }) => {
       cy.visit(`http://localhost:3000/audits/${editId}/generation`);
-      cy.get(".criterium-container").contains("Non conforme").click();
-      cy.wait("@updateResults");
+
+      cy.get(".criterium-container").contains("Non conforme");
+      cy.get(".criterium-container").contains("Erreurs et recommandations (1)").click();
 
       // 3. Copy-paste HTML content with 2 images
       cy.log("** Paste 1 image from clipboard **");
