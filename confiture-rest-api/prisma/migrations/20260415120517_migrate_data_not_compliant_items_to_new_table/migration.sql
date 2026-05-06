9e53e76672206395fb14b0d2153e7e0dd6698336
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-05-06 14:59:54 +0200

    fix script

--- confiture-rest-api/prisma/migrations/20260415120517_migrate_data_not_compliant_items_to_new_table/migration.sql
+++ confiture-rest-api/prisma/migrations/20260415120517_migrate_data_not_compliant_items_to_new_table/migration.sql
@@ -3,6 +3,3 @@ select "id", "errorDescription", "userImpact", "quickWin"
 from "CriterionResult"
 where 
   "status" = 'NOT_COMPLIANT'
-AND ("userImpact" is not null
-OR "errorDescription" is not null
-OR  "quickWin" = true)
