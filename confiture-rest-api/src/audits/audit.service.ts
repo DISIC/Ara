287ceb6fe16de2704d3aae5f7f9acf16d99ce589
Quentin Bellanger <hello@quentin-bellanger.com>
2026-05-13 16:51:34 +0200

    feat: correctly update audit auditor with a connect or create

--- confiture-rest-api/src/audits/audit.service.ts
+++ confiture-rest-api/src/audits/audit.service.ts
@@ -1886,7 +1886,6 @@ export class AuditService {
    * Transfer an audit ownership to another user and link it to its account (if any)
    */
   async transferAudit(uniqueId: string, newEmail: string) {
-    console.log("hey");
     const updatedAudit = this.prisma.$transaction(async (tx) => {
       // Get new owner info
       const user = await tx.user.findUnique({
@@ -1899,9 +1898,8 @@ export class AuditService {
         }
       });
 
-      // Update audit with new owner info if any
-      const data: Pick<Audit, "auditorEmail" | "auditorName" | "auditorOrganisation"> = {
-        auditorEmail: newEmail,
+      // Update audit with new owner info if any or reset fields
+      const data: Pick<Audit, "auditorName" | "auditorOrganisation"> = {
         auditorName: "",
         auditorOrganisation: ""
       };
@@ -1914,11 +1912,20 @@ export class AuditService {
         data.auditorOrganisation = user.orgName;
       }
 
-      // FIXME: "'insert or update on table "Audit" violates foreign key constraint "Audit_auditorEmail_fkey"'"
-      // when transfering to email without account
       return await tx.audit.update({
         where: { editUniqueId: uniqueId },
-        data,
+        data: {
+          auditorOrganisation: data.auditorOrganisation,
+          auditorName: data.auditorName,
+          auditor: {
+            connectOrCreate: {
+              where: { username: newEmail },
+              create: {
+                username: newEmail
+              }
+            }
+          }
+        },
         select: AUDIT_PRISMA_SELECT
       });
     });
