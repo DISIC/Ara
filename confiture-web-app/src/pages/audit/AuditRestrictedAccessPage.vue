5f66c739f9ab5a8b89ca8fbca83577b7833d826d
Quentin Bellanger <hello@quentin-bellanger.com>
2026-05-20 17:45:06 +0200

    create restricted page access

---
+++ confiture-web-app/src/pages/audit/AuditRestrictedAccessPage.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { useRoute } from "vue-router";
+import { useWrappedFetch } from "../../composables/useWrappedFetch";
+import { useAuditStore } from "../../store";
+
+const route = useRoute();
+const uniqueId = route.params.uniqueId as string;
+useWrappedFetch(() => auditStore.fetchAuditIfNeeded(uniqueId));
+const auditStore = useAuditStore();
+</script>
+<template>
+  <div>
+    <h1 class="fr-mb-3v">Accès restreint</h1>
+    <p>L’audit <span class="fr-text--bold fr-mb-0">« {{ auditStore.currentAudit?.procedureName }} »</span> est privé.</p>
+    <p>Pour accéder à l’audit, contactez le propriétaire de l’audit.</p>
+  </div>
+</template>
+<!--
+<style scoped>
+</style> -->
