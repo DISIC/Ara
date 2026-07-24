2f02842546e4dda3fec1cf030e2bebe2d3a152cb
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-07-24 16:48:25 +0200

    update accessibility tests with rgaa

--- cypress/e2e/accessibility.cy.ts
+++ cypress/e2e/accessibility.cy.ts
@@ -1,19 +1,35 @@
+import { checkA11y } from "./common";
+
 describe("Accessibility", () => {
   it("Has no detectable a11y violations on load", () => {
     cy.visit("http://localhost:3000");
-    cy.injectAxe();
 
-    cy.configureAxe({
-      locale: {
-        lang: "fr"
-      }
+    checkA11y();
+  });
+
+  it("Has no detectable a11y violations on dashboard page", () => {
+    cy.createTestAccount({ login: true }).then(({ username }) => {
+      cy.createTestAudit({ auditorEmail: username, isComplete: true });
+      cy.createTestAudit({ auditorEmail: username, isPristine: true });
+      cy.createTestAudit({ auditorEmail: username });
+      cy.createTestAudit({ auditorEmail: username }).as("audit");
+      cy.visit("http://localhost:3000/compte");
+
+      checkA11y();
     });
+  });
 
-    cy.checkA11y(undefined, {
-      runOnly: {
-        type: "tag",
-        values: ["rgaav4"]
-      }
+  it("Has no detectable a11y violations on generation page", () => {
+    cy.createTestAudit({ isComplete: true }).then(({ editId }) => {
+      cy.visit(`http://localhost:3000/audits/${editId}/generation/`);
+      checkA11y();
+    });
+  });
+
+  it("Has no detectable a11y violations on synthese page", () => {
+    cy.createTestAudit({ isComplete: true }).then(({ editId }) => {
+      cy.visit(`http://localhost:3000/audits/${editId}/synthese/`);
+      checkA11y();
     });
   });
 });
--- cypress/e2e/common.ts
+++ cypress/e2e/common.ts
@@ -1,3 +1,5 @@
+import { NodeResult, Result } from "axe-core";
+
 export function testTabReachByURL(slug: string) {
   cy.get(`.tabs button[data-slug="${slug}"]`)
     .should("exist")
@@ -30,4 +32,43 @@ export function testTabsWithPrevNext(slug: string, nextSlug: string) {
     .should("exist")
     .invoke("attr", "aria-selected")
     .should("eq", "true");
+}
+
+export function checkA11y() {
+  cy.injectAxe();
+
+  cy.configureAxe({
+    locale: {
+      lang: "fr"
+    },
+    reporter: "option"
+  });
+
+  cy.checkA11y(undefined, {
+    runOnly: {
+      type: "tag",
+      values: ["RGAAv4"]
+    }
+  }, terminalLog);
+}
+
+function terminalLog(violations: Result[]) {
+  cy.log(
+    "log",
+    `${violations.length} accessibility violation${
+      violations.length === 1 ? "" : "s"
+    } ${violations.length === 1 ? "was" : "were"} detected`
+  );
+
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+
+      id,
+      impact,
+      description,
+      nodes: nodes.map((value: NodeResult) => value.target).join(",")
+    })
+  );
+
+  cy.table(violationData);
 }
