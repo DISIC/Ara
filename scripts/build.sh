9be2f41c0cc42d2d87eba4b995080ce412388695
Adrien Boutigny <adrien@slash-tmp.dev>
2026-09-04 12:44:12 +0200

    use `yarn workspace focus` command to remove dev deps, disable scalingo pruning

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,20 @@ set -e
 
 echo "🚧 BUILDING... Generating API types..."
 yarn copytypes
+
 echo "🚧 BUILDING... Generating RGAA files..."
 yarn workspace confiture-web-app run generate:rgaa
-echo "🚧 BUILDING... Building apps..."
-yarn workspaces foreach --all run build
+
+echo "🚧 BUILDING... Building client app..."
+yarn workspace confiture-web-app run build
+echo "🚧 BUILDING... Building backend app..."
+yarn workspace confiture-rest-api run build
+
 echo "🚧 BUILDING... Moving frontend build to backend static folder"
 rm -rf confiture-rest-api/client
 mv confiture-web-app/dist confiture-rest-api/client
+
+echo "🚧 BUILDING... Pruning dependencies"
+yarn workspaces focus --production --all
+
 echo "✅ BUILDING DONE !"
