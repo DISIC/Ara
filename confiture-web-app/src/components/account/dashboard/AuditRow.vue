97912e91929d8701235e11bc0b9756240642fb6a
Quentin Bellanger <hello@quentin-bellanger.com>
2026-09-09 16:40:48 +0200

    feat: add share modal in audits list

--- confiture-web-app/src/components/account/dashboard/AuditRow.vue
+++ confiture-web-app/src/components/account/dashboard/AuditRow.vue
@@ -18,8 +18,9 @@ import {
 } from "../../../utils";
 import AuditProgressBar from "../../audit/AuditProgressBar.vue";
 import DuplicateModal from "../../audit/DuplicateModal.vue";
-import CopyButton from "../../ui/CopyButton.vue";
+import ShareModal from "../../audit/ShareModal.vue";
 
+import CopyButton from "../../ui/CopyButton.vue";
 import Dropdown from "../../ui/Dropdown.vue";
 
 const props = defineProps<{
@@ -80,6 +81,8 @@ function duplicateAudit(name: string) {
       duplicateModal.value?.hide();
     });
 }
+
+const shareModal = ref<InstanceType<typeof ShareModal>>();
 
 const csvExportUrl = computed(
   () => `/api/audits/${props.audit.editUniqueId}/exports/csv`
@@ -277,7 +280,7 @@ defineExpose({
           <li class="dropdown-item">
             <button
               class="fr-btn fr-btn--tertiary-no-outline fr-btn--icon-left fr-icon-user-add-line fr-m-0"
-              @click="console.log('partager')"
+              @click="shareModal?.show()"
             >
               <!-- TODO: delete badge in 1 month after merging -->
               Partager <span class="fr-badge fr-badge--sm fr-badge--yellow-moutarde fr-badge--icon-left fr-icon-checkbox-line fr-ml-1v">Nouveau</span>
@@ -371,6 +374,16 @@ defineExpose({
     :original-audit-name="audit.procedureName"
     :is-loading="isDuplicationLoading"
     @confirm="duplicateAudit"
+    @closed="
+      optionsDropdownRef?.buttonRef?.focus();
+      optionsDropdownRef?.closeOptions();
+    "
+  />
+
+  <ShareModal
+    ref="shareModal"
+    :edit-unique-id="audit.editUniqueId"
+    :audit-name="audit.procedureName"
     @closed="
       optionsDropdownRef?.buttonRef?.focus();
       optionsDropdownRef?.closeOptions();
