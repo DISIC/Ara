46bdc23689b4a6f011c9d9ac7434b2240359caa6
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-09-17 16:01:28 +0200

    clean Playwright browser cache

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "🚧 BUILDING... Cleaning Playwright browsers..."
+rm -rf ~/.cache/ms-playwright
+
 echo "🚧 BUILDING... Installing Playwright Chromium headless shell..."
 yarn workspace confiture-rest-api playwright install --only-shell
 
