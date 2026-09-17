bee9df92b1c212067863b101519ba8b6dddef02e
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-09-17 16:25:16 +0200

    log size of scalingo with playwright

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "🚧 BUILDING... Installing Playwright Chromium headless shell..."
+yarn workspace confiture-rest-api playwright install --only-shell
+
+echo "📦 Playwright cache after installation:"
+du -sh ~/.cache/ms-playwright || true
+
 echo "🚧 BUILDING... Generating API types..."
 yarn copytypes
 
@@ -9,6 +15,7 @@ yarn workspace confiture-web-app run generate:rgaa
 
 echo "🚧 BUILDING... Building client app..."
 yarn workspace confiture-web-app run build
+
 echo "🚧 BUILDING... Building backend app..."
 yarn workspace confiture-rest-api run build
 
@@ -16,8 +23,21 @@ echo "🚧 BUILDING... Moving frontend build to backend static folder"
 rm -rf confiture-rest-api/client
 mv confiture-web-app/dist confiture-rest-api/client
 
+echo "📦 node_modules before pruning:"
+du -sh node_modules || true
+
+echo "📦 Playwright cache before pruning:"
+du -sh ~/.cache/ms-playwright || true
+
 echo "🚧 BUILDING... Pruning dependencies"
-yarn workspaces focus --production --all
+yarn workspaces focus confiture-rest-api --production
+
+echo "📦 node_modules after pruning:"
+du -sh node_modules || true
+
+echo "📦 Playwright cache after pruning:"
+du -sh ~/.cache/ms-playwright || true
+
 yarn cache clean
 
 echo "✅ BUILDING DONE !"
