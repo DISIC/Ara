f2422e875f1afd6e2219c58ffd69e06fb51ab8f1
Emmanuelle ABOAF <35635211+emma11y@users.noreply.github.com>
2026-09-23 16:27:35 +0200

    Le bouton n'est pas désactivé lorsque l'usager n'a pas encore saisie la déclaration

--- confiture-web-app/src/pages/audit/AuditDeclarationPage.vue
+++ confiture-web-app/src/pages/audit/AuditDeclarationPage.vue
@@ -600,7 +600,7 @@ function confirmLeave() {
     </div>
 
     <div class="fr-mt-6w actions">
-      <button class="fr-btn" type="submit" :disabled="isPristine">
+      <button class="fr-btn" type="submit" :disabled="isPristine && auditIsPublishable">
         {{
           auditIsPublishable
             ? "Enregistrer les modifications"
