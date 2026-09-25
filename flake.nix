92fa2bdc79ee8816aa2cb2df9b71c0f8b6c52e20
Adrien Boutigny <adrien@slash-tmp.dev>
2026-09-25 12:47:22 +0200

    update README.md, flake.nix and .github/workflows/lint.yml

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Development environment with Node.js 22.14.0";
+  description = "Development environment with Node.js 24.21.0";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -11,31 +11,31 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
 
-      # Custom Node.js 22.14.0 derivation
+      # Custom Node.js 24.21.0 derivation
       nodeInfo = {
         "x86_64-linux" = {
-          url = "https://nodejs.org/dist/v22.14.0/node-v22.14.0-linux-x64.tar.xz";
+          url = "https://nodejs.org/dist/v24.21.0/node-v24.21.0-linux-x64.tar.xz";
           sha256 = "1v1p0kl4kcn55gdfy9rrzvman18hvd046fi7wk20bjwdbjx9vc39";
         };
         "aarch64-linux" = {
-          url = "https://nodejs.org/dist/v22.14.0/node-v22.14.0-linux-arm64.tar.xz";
+          url = "https://nodejs.org/dist/v24.21.0/node-v24.21.0-linux-arm64.tar.xz";
           sha256 = "0l00wfcxr6bxnmh2z8k79a3hbmr8r8y1gw390axqq3mdid9vzgq8";
         };
         "x86_64-darwin" = {
-          url = "https://nodejs.org/dist/v22.14.0/node-v22.14.0-darwin-x64.tar.xz";
+          url = "https://nodejs.org/dist/v24.21.0/node-v24.21.0-darwin-x64.tar.xz";
           sha256 = "00mzvj4069wlv449czbdahjkfv2fjv1ky74wshy80gszq88v5dfy";
         };
         "aarch64-darwin" = {
-          url = "https://nodejs.org/dist/v22.14.0/node-v22.14.0-darwin-arm64.tar.xz";
+          url = "https://nodejs.org/dist/v24.21.0/node-v24.21.0-darwin-arm64.tar.xz";
           sha256 = "09rmqhzy5p54h96l0m86f948lnj03kiv4hr7664p52af3fvmr12f";
         };
       };
 
       currentNodeInfo = nodeInfo.${system} or (throw "Unsupported system: ${system}");
 
-      nodejs-22_14_0 = pkgs.stdenv.mkDerivation rec {
+      nodejs-24_21_0 = pkgs.stdenv.mkDerivation rec {
         pname = "nodejs";
-        version = "22.14.0";
+        version = "24.21.0";
 
         src = pkgs.fetchurl {
           url = currentNodeInfo.url;
@@ -51,7 +51,7 @@
     {
       devShells.default = pkgs.mkShell {
         buildInputs = [
-          nodejs-22_14_0
+          nodejs-24_21_0
         ];
 
         nativeBuildInputs = with pkgs; [
